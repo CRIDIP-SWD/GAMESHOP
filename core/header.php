abcc5e0a801e4fcdd18395a0fba8adcd8576b1a7
CRIDIP-SWD <mmockelyn@cridip.com>
2015-12-11 00:55:12 +0100

    GAMESHOP UPDATE

--- core/header.php
+++ core/header.php
@@ -9,14 +9,14 @@
     <!-- Stylesheets
     ============================================= -->
     <link href="http://fonts.googleapis.com/css?family=Lato:300,400,400italic,600,700|Raleway:300,400,500,600,700|Crete+Round:400italic" rel="stylesheet" type="text/css" />
-    <link rel="stylesheet" href="css/bootstrap.css" type="text/css" />
-    <link rel="stylesheet" href="style.css" type="text/css" />
-    <link rel="stylesheet" href="css/dark.css" type="text/css" />
-    <link rel="stylesheet" href="css/font-icons.css" type="text/css" />
-    <link rel="stylesheet" href="css/animate.css" type="text/css" />
-    <link rel="stylesheet" href="css/magnific-popup.css" type="text/css" />
+    <link rel="stylesheet" href="<?= $constante::CSS; ?>bootstrap.css" type="text/css" />
+    <link rel="stylesheet" href="<?= $constante::ASSETS; ?>style.css" type="text/css" />
+    <link rel="stylesheet" href="<?= $constante::CSS; ?>dark.css" type="text/css" />
+    <link rel="stylesheet" href="<?= $constante::CSS; ?>font-icons.css" type="text/css" />
+    <link rel="stylesheet" href="<?= $constante::CSS; ?>animate.css" type="text/css" />
+    <link rel="stylesheet" href="<?= $constante::CSS; ?>magnific-popup.css" type="text/css" />
 
-    <link rel="stylesheet" href="css/responsive.css" type="text/css" />
+    <link rel="stylesheet" href="<?= $constante::CSS; ?>responsive.css" type="text/css" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <!--[if lt IE 9]>
     <script src="http://css3-mediaqueries-js.googlecode.com/svn/trunk/css3-mediaqueries.js"></script>
@@ -24,19 +24,19 @@
 
     <!-- External JavaScripts
 	============================================= -->
-    <script type="text/javascript" src="js/jquery.js"></script>
-    <script type="text/javascript" src="js/plugins.js"></script>
+    <script type="text/javascript" src="<?= $constante::JS; ?>jquery.js"></script>
+    <script type="text/javascript" src="<?= $constante::JS; ?>plugins.js"></script>
 
     <!-- SLIDER REVOLUTION 4.x SCRIPTS  -->
-    <script type="text/javascript" src="include/rs-plugin/js/jquery.themepunch.tools.min.js"></script>
-    <script type="text/javascript" src="include/rs-plugin/js/jquery.themepunch.revolution.min.js"></script>
+    <script type="text/javascript" src="<?= $constante::INCLUDES; ?>rs-plugin/js/jquery.themepunch.tools.min.js"></script>
+    <script type="text/javascript" src="<?= $constante::INCLUDES; ?>rs-plugin/js/jquery.themepunch.revolution.min.js"></script>
 
     <!-- SLIDER REVOLUTION 4.x CSS SETTINGS -->
-    <link rel="stylesheet" type="text/css" href="include/rs-plugin/css/settings.css" media="screen" />
+    <link rel="stylesheet" type="text/css" href="<?= $constante::INCLUDES; ?>rs-plugin/css/settings.css" media="screen" />
 
     <!-- Document Title
     ============================================= -->
-    <title>Home - Shop | Canvas</title>
+    <title><?= $constante::NOM_SITE; ?></title>
 
     <style>
 
