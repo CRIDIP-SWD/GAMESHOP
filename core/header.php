45368d5b4edce651ef8a862bccbfcd61d5c7e467
CRIDIP-SWD <mmockelyn@cridip.com>
2015-12-11 16:20:20 +0100

    GAMESHOP UPDATE

--- core/header.php
+++ core/header.php
@@ -8,7 +8,7 @@
 
     <!-- Stylesheets
     ============================================= -->
-    <link href="http://fonts.googleapis.com/css?family=Lato:300,400,400italic,600,700|Raleway:300,400,500,600,700|Crete+Round:400italic" rel="stylesheet" type="text/css" />
+    <link href="https://fonts.googleapis.com/css?family=Lato:300,400,400italic,600,700|Raleway:300,400,500,600,700|Crete+Round:400italic" rel="stylesheet" type="text/css" />
     <link rel="stylesheet" href="<?= $constante::CSS; ?>bootstrap.css" type="text/css" />
     <link rel="stylesheet" href="<?= $constante::ASSETS; ?>style.css" type="text/css" />
     <link rel="stylesheet" href="<?= $constante::CSS; ?>dark.css" type="text/css" />
@@ -19,7 +19,7 @@
     <link rel="stylesheet" href="<?= $constante::CSS; ?>responsive.css" type="text/css" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <!--[if lt IE 9]>
-    <script src="http://css3-mediaqueries-js.googlecode.com/svn/trunk/css3-mediaqueries.js"></script>
+    <script src="https://css3-mediaqueries-js.googlecode.com/svn/trunk/css3-mediaqueries.js"></script>
     <![endif]-->
 
     <!-- External JavaScripts
