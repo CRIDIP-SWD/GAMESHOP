8c7a14f8b9557d85250ea7a515c37cbfeeecf28c
CRIDIP-SWD <mmockelyn@cridip.com>
2015-12-11 01:10:51 +0100

    GAMESHOP UPDATE

--- core/classe/constante.php
+++ core/classe/constante.php
@@ -9,14 +9,14 @@ class constante
     const ENCRYPT       = "http";
     const ROOT          = self::ENCRYPT."://vps221243.ovh.net/gameshop/";
     const ASSETS        = self::ROOT."assets/";
-    const CSS           = self::ROOT.self::ASSETS."css/";
-    const IMAGES        = self::ROOT.self::ASSETS."images/";
-    const INCLUDES      = self::ROOT.self::ASSETS."include/";
-    const JS            = self::ROOT.self::ASSETS."js/";
+    const CSS           = self::ASSETS."css/";
+    const IMAGES        = self::ASSETS."images/";
+    const INCLUDES      = self::ASSETS."include/";
+    const JS            = self::ASSETS."js/";
 
     const CORE          = self::ROOT."core/";
-    const CONTROL       = self::ROOT.self::CORE."control/";
-    const CLASSE        = self::ROOT.self::CORE."classe/";
+    const CONTROL       = self::CORE."control/";
+    const CLASSE        = self::CORE."classe/";
 
     const NOM_SITE      = "GAMESHOP";
 }
