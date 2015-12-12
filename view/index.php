d56ab848e8c9dc372a429a5d5b943b98d870a6fa
CRIDIP-SWD <mmockelyn@cridip.com>
2015-12-12 13:57:05 +0100

    GAMESHOP UPDATE

--- view/index.php
+++ view/index.php
@@ -5,272 +5,39 @@
         <button type="button" class="btn btn-block btn-default">offre spécizl</button>
     </div>
     <div class="col-md-9">
-        <section id="slider" class="slider-parallax revslider-wrap ohidden clearfix">
+        <section id="slider" class="boxed-slider">
 
-            <!--
-            #################################
-                - THEMEPUNCH BANNER -
-            #################################
-            -->
-            <div class="tp-banner-container">
-                <div class="tp-banner" >
-                    <ul>    <!-- SLIDE  -->
-                        <li data-transition="fade" data-slotamount="1" data-masterspeed="1500" data-delay="10000" data-saveperformance="off" data-title="Latest Collections" style="background-color: #F6F6F6;">
-                            <!-- LAYERS -->
+            <div class="container clearfix">
 
-                            <!-- LAYER NR. 2 -->
-                            <div class="tp-caption customin ltl tp-resizeme revo-slider-caps-text uppercase"
-                                 data-x="100"
-                                 data-y="50"
-                                 data-customin="x:-200;y:0;z:0;rotationZ:0;scaleX:1;scaleY:1;skewX:0;skewY:0;opacity:0;transformPerspective:200;transformOrigin:50% 0%;"
-                                 data-speed="400"
-                                 data-start="1000"
-                                 data-easing="easeOutQuad"
-                                 data-splitin="none"
-                                 data-splitout="none"
-                                 data-elementdelay="0.01"
-                                 data-endelementdelay="0.1"
-                                 data-endspeed="1000"
-                                 data-endeasing="Power4.easeIn" style=""><img src="<?= $constante->getUrl(array('images/')); ?>slider/rev/shop/girl1.jpg" alt="Girl"></div>
+                <div id="oc-slider" class="owl-carousel">
 
-                            <div class="tp-caption customin ltl tp-resizeme revo-slider-caps-text uppercase"
-                                 data-x="570"
-                                 data-y="75"
-                                 data-customin="x:0;y:150;z:0;rotationZ:0;scaleX:1.3;scaleY:1;skewX:0;skewY:0;opacity:0;transformPerspective:200;transformOrigin:50% 0%;"
-                                 data-speed="700"
-                                 data-start="1000"
-                                 data-easing="easeOutQuad"
-                                 data-splitin="none"
-                                 data-splitout="none"
-                                 data-elementdelay="0.01"
-                                 data-endelementdelay="0.1"
-                                 data-endspeed="1000"
-                                 data-endeasing="Power4.easeIn" style=" color: #333;">Get your Shopping Bags Ready</div>
+                    <a href="#"><img src="images/slider/boxed/2.jpg" alt="Slider"></a>
+                    <a href="#"><img src="images/slider/boxed/3.jpg" alt="Slider"></a>
+                    <a href="#"><img src="images/slider/boxed/4.jpg" alt="Slider"></a>
+                    <a href="#"><img src="images/slider/boxed/5.jpg" alt="Slider"></a>
 
-                            <div class="tp-caption customin ltl tp-resizeme revo-slider-emphasis-text nopadding noborder"
-                                 data-x="570"
-                                 data-y="105"
-                                 data-customin="x:0;y:150;z:0;rotationZ:0;scaleX:1.3;scaleY:1;skewX:0;skewY:0;opacity:0;transformPerspective:200;transformOrigin:50% 0%;"
-                                 data-speed="700"
-                                 data-start="1200"
-                                 data-easing="easeOutQuad"
-                                 data-splitin="none"
-                                 data-splitout="none"
-                                 data-elementdelay="0.01"
-                                 data-endelementdelay="0.1"
-                                 data-endspeed="1000"
-                                 data-endeasing="Power4.easeIn" style=" color: #333; max-width: 430px; white-space: normal; line-height: 1.15;">Latest Fashion Collections</div>
-
-                            <div class="tp-caption customin ltl tp-resizeme revo-slider-desc-text tleft"
-                                 data-x="570"
-                                 data-y="275"
-                                 data-customin="x:0;y:150;z:0;rotationZ:0;scaleX:1.3;scaleY:1;skewX:0;skewY:0;opacity:0;transformPerspective:200;transformOrigin:50% 0%;"
-                                 data-speed="700"
-                                 data-start="1400"
-                                 data-easing="easeOutQuad"
-                                 data-splitin="none"
-                                 data-splitout="none"
-                                 data-elementdelay="0.01"
-                                 data-endelementdelay="0.1"
-                                 data-endspeed="1000"
-                                 data-endeasing="Power4.easeIn" style=" color: #333; max-width: 550px; white-space: normal;">We have created a Design that looks Awesome, performs Brilliantly &amp; senses Orientations.</div>
-
-                            <div class="tp-caption customin ltl tp-resizeme"
-                                 data-x="570"
-                                 data-y="375"
-                                 data-customin="x:0;y:150;z:0;rotationZ:0;scaleX:1.3;scaleY:1;skewX:0;skewY:0;opacity:0;transformPerspective:200;transformOrigin:50% 0%;"
-                                 data-speed="700"
-                                 data-start="1550"
-                                 data-easing="easeOutQuad"
-                                 data-splitin="none"
-                                 data-splitout="none"
-                                 data-elementdelay="0.01"
-                                 data-endelementdelay="0.1"
-                                 data-endspeed="1000"
-                                 data-endeasing="Power4.easeIn" style=""><a href="#" class="button button-border button-large button-rounded tright nomargin"><span>Start Shopping</span> <i class="icon-angle-right"></i></a></div>
-
-                        </li>
-                        <!-- SLIDE  -->
-                        <li data-transition="slideup" data-slotamount="1" data-masterspeed="1500" data-delay="10000"  data-saveperformance="off"  data-title="Messenger bags" style="background-color: #E9E8E3;">
-                            <!-- LAYERS -->
-
-                            <div class="tp-caption customin ltl tp-resizeme revo-slider-caps-text uppercase"
-                                 data-x="630"
-                                 data-y="78"
-                                 data-customin="x:250;y:0;z:0;rotationZ:0;scaleX:1;scaleY:1;skewX:0;skewY:0;opacity:0;transformPerspective:200;transformOrigin:50% 0%;"
-                                 data-speed="400"
-                                 data-start="1000"
-                                 data-easing="easeOutQuad"
-                                 data-splitin="none"
-                                 data-splitout="none"
-                                 data-elementdelay="0.01"
-                                 data-endelementdelay="0.1"
-                                 data-endspeed="1000"
-                                 data-endeasing="Power4.easeIn" style=""><img src="<?= $constante->getUrl(array('images/')); ?>slider/rev/shop/bag.png" alt="Bag"></div>
-
-                            <!-- LAYER NR. 2 -->
-                            <div class="tp-caption customin ltl tp-resizeme revo-slider-caps-text uppercase"
-                                 data-x="0"
-                                 data-y="110"
-                                 data-customin="x:0;y:150;z:0;rotationZ:0;scaleX:1.3;scaleY:1;skewX:0;skewY:0;opacity:0;transformPerspective:200;transformOrigin:50% 0%;"
-                                 data-speed="700"
-                                 data-start="1000"
-                                 data-easing="easeOutQuad"
-                                 data-splitin="none"
-                                 data-splitout="none"
-                                 data-elementdelay="0.01"
-                                 data-endelementdelay="0.1"
-                                 data-endspeed="1000"
-                                 data-endeasing="Power4.easeIn" style=" color: #333;">Buy Stylish Bags at Discounted Prices</div>
-
-                            <div class="tp-caption customin ltl tp-resizeme revo-slider-emphasis-text nopadding noborder"
-                                 data-x="0"
-                                 data-y="140"
-                                 data-customin="x:0;y:150;z:0;rotationZ:0;scaleX:1.3;scaleY:1;skewX:0;skewY:0;opacity:0;transformPerspective:200;transformOrigin:50% 0%;"
-                                 data-speed="700"
-                                 data-start="1200"
-                                 data-easing="easeOutQuad"
-                                 data-splitin="none"
-                                 data-splitout="none"
-                                 data-elementdelay="0.01"
-                                 data-endelementdelay="0.1"
-                                 data-endspeed="1000"
-                                 data-endeasing="Power4.easeIn" style=" color: #333; white-space: normal; line-height: 1.15;">Messenger Bags</div>
-
-                            <div class="tp-caption customin ltl tp-resizeme revo-slider-desc-text tleft"
-                                 data-x="0"
-                                 data-y="240"
-                                 data-customin="x:0;y:150;z:0;rotationZ:0;scaleX:1.3;scaleY:1;skewX:0;skewY:0;opacity:0;transformPerspective:200;transformOrigin:50% 0%;"
-                                 data-speed="700"
-                                 data-start="1400"
-                                 data-easing="easeOutQuad"
-                                 data-splitin="none"
-                                 data-splitout="none"
-                                 data-elementdelay="0.01"
-                                 data-endelementdelay="0.1"
-                                 data-endspeed="1000"
-                                 data-endeasing="Power4.easeIn" style=" color: #333; max-width: 550px; white-space: normal;">Grantees insurmountable challenges invest protect, growth improving quality social entrepreneurship.</div>
-
-                            <div class="tp-caption customin ltl tp-resizeme"
-                                 data-x="0"
-                                 data-y="340"
-                                 data-customin="x:0;y:150;z:0;rotationZ:0;scaleX:1.3;scaleY:1;skewX:0;skewY:0;opacity:0;transformPerspective:200;transformOrigin:50% 0%;"
-                                 data-speed="700"
-                                 data-start="1550"
-                                 data-easing="easeOutQuad"
-                                 data-splitin="none"
-                                 data-splitout="none"
-                                 data-elementdelay="0.01"
-                                 data-endelementdelay="0.1"
-                                 data-endspeed="1000"
-                                 data-endeasing="Power4.easeIn" style=""><a href="#" class="button button-border button-large button-rounded tright nomargin"><span>Start Shopping</span> <i class="icon-angle-right"></i></a></div>
-
-                            <div class="tp-caption customin utb tp-resizeme revo-slider-caps-text uppercase"
-                                 data-x="510"
-                                 data-y="0"
-                                 data-customin="x:0;y:-236;z:0;rotationZ:0;skewX:0;skewY:0;opacity:0;transformPerspective:200;transformOrigin:50% 0%;"
-                                 data-speed="600"
-                                 data-start="2100"
-                                 data-easing="easeOutQuad"
-                                 data-splitin="none"
-                                 data-splitout="none"
-                                 data-elementdelay="0.01"
-                                 data-endelementdelay="0.1"
-                                 data-endspeed="1000"
-                                 data-endeasing="Power4.easeIn" style=""><img src="<?= $constante->getUrl(array('images/')); ?>slider/rev/shop/tag.png" alt="Bag"></div>
-
-                        </li>
-                    </ul>
                 </div>
-            </div>
 
-            <script type="text/javascript">
+                <script>
 
-                jQuery(document).ready(function() {
+                    jQuery(document).ready(function($) {
 
-                    jQuery('.tp-banner').show().revolution(
-                        {
-                            dottedOverlay:"none",
-                            delay:16000,
-                            startwidth:1140,
-                            startheight:500,
-                            hideThumbs:200,
+                        var ocSlider = $("#oc-slider");
 
-                            thumbWidth:100,
-                            thumbHeight:50,
-                            thumbAmount:5,
-
-                            navigationType:"none",
-                            navigationArrows:"solo",
-                            navigationStyle:"preview2",
-
-                            touchenabled:"on",
-                            onHoverStop:"on",
-
-                            swipe_velocity: 0.7,
-                            swipe_min_touches: 1,
-                            swipe_max_touches: 1,
-                            drag_block_vertical: false,
-
-                            parallax:"mouse",
-                            parallaxBgFreeze:"on",
-                            parallaxLevels:[7,4,3,2,5,4,3,2,1,0],
-
-                            keyboardNavigation:"off",
-
-                            navigationHAlign:"center",
-                            navigationVAlign:"bottom",
-                            navigationHOffset:0,
-                            navigationVOffset:20,
-
-                            soloArrowLeftHalign:"left",
-                            soloArrowLeftValign:"center",
-                            soloArrowLeftHOffset:20,
-                            soloArrowLeftVOffset:0,
-
-                            soloArrowRightHalign:"right",
-                            soloArrowRightValign:"center",
-                            soloArrowRightHOffset:20,
-                            soloArrowRightVOffset:0,
-
-                            shadow:0,
-                            fullWidth:"on",
-                            fullScreen:"off",
-
-                            spinner:"spinner4",
-
-                            stopLoop:"off",
-                            stopAfterLoops:-1,
-                            stopAtSlide:-1,
-
-                            shuffle:"off",
-
-                            autoHeight:"off",
-                            forceFullWidth:"off",
-
-
-
-                            hideThumbsOnMobile:"off",
-                            hideNavDelayOnMobile:1500,
-                            hideBulletsOnMobile:"off",
-                            hideArrowsOnMobile:"off",
-                            hideThumbsUnderResolution:0,
-
-                            hideSliderAtLimit:0,
-                            hideCaptionAtLimit:0,
-                            hideAllCaptionAtLilmit:0,
-                            startWithSlide:0,
-                            fullScreenOffsetContainer: ".header"
+                        ocSlider.owlCarousel({
+                            items: 1,
+                            nav: true,
+                            navText : ['<i class="icon-angle-left"></i>','<i class="icon-angle-right"></i>'],
+                            animateOut: 'fadeOut',
+                            animateIn: 'zoomIn',
+                            smartSpeed: 450
                         });
 
+                    });
 
+                </script>
 
-
-                }); //ready
-
-            </script>
-
-            <!-- END REVOLUTION SLIDER -->
+            </div>
 
         </section>
     </div>
