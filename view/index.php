841878b1e1cd696cf6d644dcc93c8abb1b4d088d
CRIDIP-SWD <mmockelyn@cridip.com>
2015-12-12 13:36:38 +0100

    GAMESHOP UPDATE

--- view/index.php
+++ view/index.php
@@ -99,7 +99,7 @@
                          data-elementdelay="0.01"
                          data-endelementdelay="0.1"
                          data-endspeed="1000"
-                         data-endeasing="Power4.easeIn" style=""><img src="images/slider/rev/shop/bag.png" alt="Bag"></div>
+                         data-endeasing="Power4.easeIn" style=""><img src="<?= $constante->getUrl(array('images/')); ?>slider/rev/shop/bag.png" alt="Bag"></div>
 
                     <!-- LAYER NR. 2 -->
                     <div class="tp-caption customin ltl tp-resizeme revo-slider-caps-text uppercase"
@@ -170,7 +170,7 @@
                          data-elementdelay="0.01"
                          data-endelementdelay="0.1"
                          data-endspeed="1000"
-                         data-endeasing="Power4.easeIn" style=""><img src="images/slider/rev/shop/tag.png" alt="Bag"></div>
+                         data-endeasing="Power4.easeIn" style=""><img src="<?= $constante->getUrl(array('images/')); ?>slider/rev/shop/tag.png" alt="Bag"></div>
 
                 </li>
             </ul>
