b12dd968b96d0ec1ff7f080eb5a6dbe8509d678b
CRIDIP-SWD <mmockelyn@cridip.com>
2015-12-12 13:34:35 +0100

    GAMESHOP UPDATE

--- view/index.php
+++ view/index.php
@@ -24,7 +24,7 @@
                          data-elementdelay="0.01"
                          data-endelementdelay="0.1"
                          data-endspeed="1000"
-                         data-endeasing="Power4.easeIn" style=""><img src="images/slider/rev/shop/girl1.jpg" alt="Girl"></div>
+                         data-endeasing="Power4.easeIn" style=""><img src="<?= $constante->getUrl(array('images/')); ?>slider/rev/shop/girl1.jpg" alt="Girl"></div>
 
                     <div class="tp-caption customin ltl tp-resizeme revo-slider-caps-text uppercase"
                          data-x="570"
