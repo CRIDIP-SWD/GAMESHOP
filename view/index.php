1cfcb0c1e5609c5ea8ebbf731defded0a5e8c7b7
CRIDIP-SWD <mmockelyn@cridip.com>
2015-12-14 00:58:29 +0100

    GAMESHOP UPDATE

--- view/index.php
+++ view/index.php
@@ -347,6 +347,9 @@
                     <div class="col-md-4">
                         <a href="" class="banner-categorie"><img src="<?= $constante->getUrl(array('images/')); ?>shop/banners/xbox-one-banner.png" class="img-responsive" alt="PS4 Catégorie" /></a>
                     </div>
+                    <div class="col-md-4">
+                        <a href="" class="banner-categorie"><img src="<?= $constante->getUrl(array('images/')); ?>shop/banners/box-office-banner.png" class="img-responsive" alt="PS4 Catégorie" /></a>
+                    </div>
                 </div>
             </div>
             <div class="tabs topmargin-lg clearfix" id="tab-3">
