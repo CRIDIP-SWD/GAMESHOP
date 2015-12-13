c812ff94c70f3ee29f3d822198d62b5df6a9f682
CRIDIP-SWD <mmockelyn@cridip.com>
2015-12-13 23:00:36 +0100

    GAMESHOP UPDATE

--- view/index.php
+++ view/index.php
@@ -339,6 +339,23 @@
 <section id="content">
     <div class="content-wrap">
         <div class="container clearfix">
+            <div class="col-md-8 nopadding">
+
+                <div class="col-md-6 noleftpadding bottommargin-sm">
+                    <a href="#"><img src="<?= $constante->getUrl(array('images/')); ?>shop/banners/2.jpg" alt="Image"></a>
+                </div>
+
+                <div class="col-md-6 noleftpadding bottommargin-sm">
+                    <a href="#"><img src="<?= $constante->getUrl(array('images/')); ?>shop/banners/8.jpg" alt="Image"></a>
+                </div>
+
+                <div class="clear"></div>
+
+                <div class="col-md-12 noleftpadding">
+                    <a href="#"><img src="<?= $constante->getUrl(array('images/')); ?>shop/banners/4.jpg" alt="Image"></a>
+                </div>
+
+            </div>
             <div class="tabs topmargin-lg clearfix" id="tab-3">
 
                 <ul class="tab-nav clearfix">
