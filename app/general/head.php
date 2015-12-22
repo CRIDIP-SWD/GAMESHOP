b74d2ef3aaa8a40ef0fe439f29a0f9c3746c5e7e
CRIDIP-SWD <mmockelyn@cridip.com>
2015-12-22 23:51:00 +0100

    GAMESHOP UPDATE

--- app/general/head.php
+++ app/general/head.php
@@ -13,7 +13,7 @@ class head
 {
     public function count_subcategorie($idcategorie)
     {
-        $sql = mysql_query("SELECT COUNT(id) FROM subcategorie WHERE idcategorie = '$idcategorie'")or die(mysql_error());
+        $sql = mysql_query("SELECT COUNT(id) FROM subcategorie WHERE subcategorie.idcategorie = '$idcategorie'")or die(mysql_error());
         $count = mysql_result($sql, 0);
         return $count;
     }
