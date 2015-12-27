4384c3392c58f98e6e0ee85c030e228cb7b05f0d
CRIDIP-SWD <mmockelyn@cridip.com>
2015-12-27 04:24:52 +0100

    GAMESHOP UPDATE

--- assets/include/ajax/shop-item.php
+++ assets/include/ajax/shop-item.php
@@ -12,9 +12,7 @@ $produit = mysql_fetch_array($sql_produit);
                     <div class="ajax-modal-title">
                         <h2><?= $produit['designation']; ?></h2>
                         <span class="game-font">
-                            <?php if($produit['idcategorie']): ?>
-                                <img src="<?= $constante->getUrl(array(), false, true); ?>marque/icon-<?= $produit['images_cat']; ?>.png" />
-                            <?php endif; ?>
+                            <img src="<?= $constante->getUrl(array(), false, true); ?>marque/icon-<?= $produit['images_cat']; ?>.png" />
                         </span>
                     </div>
 
@@ -22,27 +20,12 @@ $produit = mysql_fetch_array($sql_produit);
 
                         <div class="col_half nobottommargin">
                             <div class="product-image">
-                                <div class="fslider" data-pagi="false">
-                                    <div class="flexslider">
-                                        <div class="slider-wrap">
-                                            <div class="slide"><a href="images/shop/dress/3.jpg" title="Pink Printed Dress - Front View"><img src="images/shop/dress/3.jpg" alt="Pink Printed Dress"></a></div>
-                                            <div class="slide"><a href="images/shop/dress/3-1.jpg" title="Pink Printed Dress - Side View"><img src="images/shop/dress/3-1.jpg" alt="Pink Printed Dress"></a></div>
-                                            <div class="slide"><a href="images/shop/dress/3-2.jpg" title="Pink Printed Dress - Back View"><img src="images/shop/dress/3-2.jpg" alt="Pink Printed Dress"></a></div>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="sale-flash">Sale!</div>
+                                <a href="#"><img src="<?= $constante->getUrl(array(), false, true); ?>produit/cards/<?= $produit['ref_produit']; ?>.jpg" class="img-responsive" height="360" alt="Checked Short Dress"></a>
+                                <!--<div class="sale-flash">Sale!</div>-->
                             </div>
                         </div>
                         <div class="col_half nobottommargin col_last product-desc">
-                            <div class="product-price"><del>$39.99</del> <ins>$24.99</ins></div>
-                            <div class="product-rating">
-                                <i class="icon-star3"></i>
-                                <i class="icon-star3"></i>
-                                <i class="icon-star3"></i>
-                                <i class="icon-star-half-full"></i>
-                                <i class="icon-star-empty"></i>
-                            </div>
+                            <div class="product-price"><ins><?= number_format($produit['prix_vente'], 2, ',', ' ')." €"; ?></ins></div>
                             <div class="clear"></div>
                             <div class="line"></div>
 
@@ -54,13 +37,13 @@ $produit = mysql_fetch_array($sql_produit);
                                     <input type="text" step="1" min="1"  name="quantity" value="1" title="Qty" class="qty" size="4" />
                                     <input type="button" value="+" class="plus">
                                 </div>
-                                <button type="submit" class="add-to-cart button nomargin">Add to cart</button>
+                                <button type="submit" class="add-to-cart button nomargin">Ajouter au Panier</button>
                             </form><!-- Product Single - Quantity & Cart Button End -->
 
                             <div class="clear"></div>
                             <div class="line"></div>
-                            <p>Libero velit id eaque ex quae laboriosam nulla optio doloribus! Perspiciatis, libero, neque, perferendis at nisi optio dolor!</p>
-                            <ul class="iconlist">
+                            <p><?= html_entity_decode($produit['short_description']); ?></p>
+                            <!--<ul class="iconlist">
                                 <li><i class="icon-caret-right"></i> Dynamic Color Options</li>
                                 <li><i class="icon-caret-right"></i> Lots of Size Options</li>
                                 <li><i class="icon-caret-right"></i> 30-Day Return Policy</li>
@@ -71,7 +54,7 @@ $produit = mysql_fetch_array($sql_produit);
                                     <span class="posted_in">Category: <a href="http://localhost/offbeat/wp/product-category/shoes/" rel="tag">Shoes</a>.</span>
                                     <span class="tagged_as">Tags: <a href="http://dante.swiftideas.net/product-tag/barena/" rel="tag">Barena</a>, <a href="http://dante.swiftideas.net/product-tag/blazers/" rel="tag">Blazers</a>, <a href="http://dante.swiftideas.net/product-tag/tailoring/" rel="tag">Tailoring</a>, <a href="http://dante.swiftideas.net/product-tag/unconstructed/" rel="tag">Unconstructed</a>.</span>
                                 </div>
-                            </div>
+                            </div>-->
                         </div>
 
                     </div>
