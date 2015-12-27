46aaf320ee9535127420fb1d30533bc439cf1196
CRIDIP-SWD <mmockelyn@cridip.com>
2015-12-27 04:58:23 +0100

    GAMESHOP UPDATE

--- view/categorie.php
+++ view/categorie.php
@@ -70,13 +70,7 @@
 
                                 <h4><?= $categorie['designation_cat']; ?></h4>
                                 <ul>
-                                    <li><a href="#">Shirts</a></li>
-                                    <li><a href="#">Pants</a></li>
-                                    <li><a href="#">Tshirts</a></li>
-                                    <li><a href="#">Sunglasses</a></li>
-                                    <li><a href="#">Shoes</a></li>
-                                    <li><a href="#">Bags</a></li>
-                                    <li><a href="#">Watches</a></li>
+                                    <li class="active"><a href="#">Shirts</a></li>
                                 </ul>
 
                             </div>
