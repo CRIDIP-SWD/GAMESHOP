e4ca4ec7a64a23b65b46be68ab3ecb27dd4c2c35
CRIDIP-SWD <mmockelyn@cridip.com>
2015-12-27 01:41:02 +0100

    GAMESHOP UPDATE

--- view/index.php
+++ view/index.php
@@ -227,7 +227,7 @@
                 <li data-transition="boxslide" data-slotamout="7" data-link="#">
                     <img src="<?= $constante->getUrl('', false, true); ?>slides/nuns4.jpg">
                     <div class="caption sft" style="font-size: 56px ;color: white" data-x="0" data-y="500" data-speed="700" data-start="1700" data-easing="easeOutLeft">NARUTO ULTIMATE<br> NINJA STORM 4</div>
-                    <div class="caption sft" style="font-size: 56px;" data-x="800" data-y="500" data-speed="700" data-start="2500" data-easing="easeOutLeft"><a href="#" class="btn btn-primary btn-lg"><i class="icon-line2-basket-loaded"></i> <span style="font-size: 56px;">TEST</span></a></div>
+                    <div class="caption sft" style="font-size: 56px; color: #fff;" data-x="800" data-y="500" data-speed="700" data-start="2500" data-easing="easeOutLeft"><a href="#" class="btn btn-primary btn-lg"><i class="icon-line2-basket-loaded icon-4x"></i> <span style="font-size: 35px;">Précommander Maintenant</span></a></div>
                 </li>
             </ul>
 
