7cc1d1b99f2ca2ab5e75ad7ce604a19b5c735a59
CRIDIP-SWD <mmockelyn@cridip.com>
2015-12-27 22:53:14 +0100

    GAMESHOP UPDATE

--- app/general/categorie.php
+++ app/general/categorie.php
@@ -13,7 +13,7 @@ class categorie
 {
     public function count_categorie($idcategorie)
     {
-        $sql = mysql_query("SELECT COUNT(produits.ref_produit) FROM produits, produits_categorie, categorie WHERE produits_categorie.ref_produit = produits.ref_produit
+        $sql = mysql_query("SELECT COUNT(produits_categorie.ref_produit) FROM produits, produits_categorie, categorie WHERE produits_categorie.ref_produit = produits.ref_produit
                                            AND produits_categorie.idcategorie = categorie.id
                                            AND produits_categorie.idcategorie = '$idcategorie'")or die(mysql_error());
         $res = mysql_result($sql, 0);
@@ -22,7 +22,7 @@ class categorie
 
     public function count_sub($idsubcategorie)
     {
-        $sql = mysql_query("SELECT COUNT(produits.ref_produit) FROM produits, produits_subcategorie, subcategorie WHERE produits_subcategorie.ref_produit = produits.ref_produit
+        $sql = mysql_query("SELECT COUNT(produits_subcategorie.ref_produit) FROM produits, produits_subcategorie, subcategorie WHERE produits_subcategorie.ref_produit = produits.ref_produit
                                                         AND produits_subcategorie.idsubcategorie = subcategorie.id
                                                         AND produits_subcategorie.idsubcategorie = '$idsubcategorie'")or die(mysql_error());
         $res = mysql_result($sql, 0);
