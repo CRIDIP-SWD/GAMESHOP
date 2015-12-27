d8f94f88a1c5e6cd9adf012a831e566ae61234c5
CRIDIP-SWD <mmockelyn@cridip.com>
2015-12-27 01:46:16 +0100

    GAMESHOP UPDATE

--- view/index.php
+++ view/index.php
@@ -229,8 +229,14 @@
                     <div class="caption sft" style="font-size: 56px ;color: white" data-x="0" data-y="500" data-speed="700" data-start="1700" data-easing="easeOutLeft">NARUTO ULTIMATE<br> NINJA STORM 4</div>
                     <div class="caption sft" style="font-size: 56px; color: #fff;" data-x="800" data-y="500" data-speed="700" data-start="2500" data-easing="easeOutLeft">
                         <div class="btn-group">
-                            <button class="btn btn-default btn-lg"><i class="icon-4x icon-line2-basket-loaded"></i></button>
-                            <button class="btn btn-default btn-lg" style="font-size: 25px;">Précommander Maintenant</button>
+                            <button class="btn btn-default btn-lg"><i class="icon-line2-basket-loaded"></i></button>
+                            <button class="btn btn-default btn-lg">Précommander Maintenant sur PS4</button>
+                        </div>
+                    </div>
+                    <div class="caption sft" style="font-size: 56px; color: #fff;" data-x="800" data-y="700" data-speed="700" data-start="2500" data-easing="easeOutLeft">
+                        <div class="btn-group">
+                            <button class="btn btn-primary btn-lg"><i class="icon-line2-basket-loaded"></i></button>
+                            <button class="btn btn-primary btn-lg">Précommander Maintenant sur Xbox One</button>
                         </div>
                     </div>
                 </li>
