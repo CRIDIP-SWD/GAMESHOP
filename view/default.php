c7eec13034349f56b7913916fe5a88b7eba2ec00
CRIDIP-SWD <mmockelyn@cridip.com>
2015-12-27 23:07:41 +0100

    GAMESHOP UPDATE

--- view/default.php
+++ view/default.php
@@ -144,7 +144,7 @@ session_start();
                 <!-- Logo
                 ============================================= -->
                 <div id="logo">
-                    <a href="index.html" class="standard-logo" data-dark-logo="images/logo-dark.png"><img src="assets/images/logo.png" alt="Canvas Logo"></a>
+                    <a href="index.php?view=index" class="standard-logo" data-dark-logo="images/logo-dark.png"><img src="assets/images/logo.png" alt="Canvas Logo"></a>
                     <a href="index.html" class="retina-logo" data-dark-logo="images/logo-dark@2x.png"><img src="images/logo@2x.png" alt="Canvas Logo"></a>
                 </div><!-- #logo end -->
 
