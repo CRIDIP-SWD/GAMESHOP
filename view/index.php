d950880c786b42001000e93c98b7682db098c110
CRIDIP-SWD <mmockelyn@cridip.com>
2015-12-27 03:53:18 +0100

    GAMESHOP UPDATE

--- view/index.php
+++ view/index.php
@@ -190,7 +190,7 @@
                                     <!--<div class="sale-flash">50% Off*</div>-->
                                     <div class="product-overlay">
                                         <a href="#" class="add-to-cart"><i class="icon-shopping-cart"></i><span> Ajouter au panier</span></a>
-                                        <a href="assets/include/ajax/shop-item.html" class="item-quick-view" data-lightbox="ajax"><i class="icon-zoom-in2"></i><span> Voir</span></a>
+                                        <a href="assets/include/ajax/shop-item.php" class="item-quick-view" data-lightbox="ajax"><i class="icon-zoom-in2"></i><span> Voir</span></a>
                                     </div>
                                 </div>
                                 <div class="product-desc">
@@ -317,7 +317,7 @@
                                     <a href="#"><img src="<?= $constante->getUrl(array(), false, true); ?>produit/cards/<?= $preco['ref_produit']; ?>.jpg" class="img-responsive" height="360" alt="Checked Short Dress"></a>
                                     <div class="product-overlay">
                                         <a href="#" class="add-to-cart"><i class="icon-shopping-cart"></i><span> Ajouter au panier</span></a>
-                                        <a href="assets/include/ajax/shop-item.html" class="item-quick-view" data-lightbox="ajax"><i class="icon-zoom-in2"></i><span> Voir</span></a>
+                                        <a href="assets/include/ajax/shop-item.php" class="item-quick-view" data-lightbox="ajax"><i class="icon-zoom-in2"></i><span> Voir</span></a>
                                     </div>
                                 </div>
                                 <div class="product-desc">
