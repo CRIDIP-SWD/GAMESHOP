4f80ca816f99deff91b5ef3465bd8e1b124c9c5f
CRIDIP-SWD <mmockelyn@cridip.com>
2015-12-29 18:19:39 +0100

    GAMESHOP UPDATE

--- view/profil.php
+++ view/profil.php
@@ -5,10 +5,9 @@ if($_SESSION['logged'] == false) {
 ?>
 <!-- Page Title
 		============================================= -->
-<section id="page-title">
+<section id="page-title" class="page-title-parallax page-title-dark" style="background-image: url('<?= $constante->getUrl(array(), false, true) ?>produit/banner/banner_ps4.jpg'); padding: 120px 0;" data-stellar-background-ratio="0.3">
 
     <div class="container clearfix">
-        <img src="" />
         <h1>MON COMPTE</h1>
         <span>Bienvenue</span>
         <ol class="breadcrumb">
