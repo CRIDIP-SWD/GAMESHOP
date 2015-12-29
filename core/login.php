28843233bc5cd3f271156047885b2821294c8c45
CRIDIP-SWD <mmockelyn@cridip.com>
2015-12-29 01:06:48 +0100

    GAMESHOP UPDATE

--- core/login.php
+++ core/login.php
@@ -46,7 +46,201 @@ if(isset($_POST['action']) && $_POST['action'] == 'reset-password-1')
     if($client != 0)
     {
         $token = $fonction->gen_token();
+        $to = $email;
+        $sujet = "GAMESHOP - 1/2 Réinitialisation de votre mote de passe";
+        $headers = 'Mime-Version: 1.0'."\r\n";
+        $headers .= 'Content-type: text/html; charset=utf-8'."\r\n";
+        $headers .= "\r\n";
+        ob_start();
+        ?>
+        <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+        <html xmlns:v="urn:shemas-microsoft-com:vml">
+        <head>
+            <meta http-equiv="content-type" content="text/html; charset=utf-8">
+            <meta name="viewport" content="width=device-width; initial-scale=1.0; maximum-scale=1.0">
+            <title>Réinitialisation de votre mot de passe 1/2</title>
+            <link href='https://fonts.googleapis.com/css?family=Product+Sans' rel='stylesheet' type='text/css'>
+            <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+        </head>
+        <body leftmargin="0" topmargin="0" marginwidth="0" marginheight="0">
 
+        <!--TABLE HEAD-->
+        <table bgcolor="#131448" width="100%" border="0" cellpadding="0" cellspacing="0">
+            <tbody>
+            <tr>
+                <td background="http://imgur.com/kBStm0X" bgcolor="#373e80" valign="top">
+                    <!--[if gte mso 9]>
+                    <v:rect xmlns:v="urn:schemas-microsoft-com:vml" fill="true" stroke="false" style="mso-width-percent:1000;">
+                        <v:fill type="tile" src="http://imgur.com/kBStm0X" color="#373e80" />
+                        <v:textbox style="mso-fit-shape-to-text:true" inset="0,0,0,0">
+                    <![endif]-->
+                    <div>
+                        <table align="center" width="590" border="0" cellpadding="0" cellspacing="0">
+                            <tbody>
+                            <tr>
+                                <td height="70" style="font-size: 30px; line-height: 30px;">&nbsp;</td>
+                            </tr>
+                            <tr>
+                                <td align="center" style="text-align: center;">
+                                    <a href="http://gestcom.cridip.com">
+                                        <img src="<?= $constante->getUrl(array('images/'), true, false); ?>logo.png" width="240" border="0" alt="Logo GAMESHOP" />
+                                    </a>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td height="30" style="font-size: 30px; line-height: 30px;">&nbsp;</td>
+                            </tr>
+                            <tr>
+                                <td align="center" style="text-align: center; font-size: 25px; color: white; mso-line-height-rule: exactly; line-height: 30px; font-family: 'Product Sans',Helvetica, Arial, sans-serif">
+                                    Réinitialisation de Votre Mot de Passe<br>1 / 2
+                                </td>
+                            </tr>
+                            <tr>
+                                <td height="30" style="font-size: 30px; line-height: 30px;">&nbsp;</td>
+                            </tr>
+                            <tr>
+                                <td align="center">
+                                    <table align="center" width="45" border="0" cellpadding="0" cellspacing="0" bgcolor="#1eff83">
+                                        <tbody>
+                                        <tr>
+                                            <td height="4" style="font-size: 4px; line-height: 4px;"></td>
+                                        </tr>
+                                        </tbody>
+                                    </table>
+                                </td>
+                            </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                    <!--[if gte mso 9]>
+                    </v:textbox>
+                    </v:rect>
+                    <![endif]-->
+                </td>
+            </tr>
+            </tbody>
+        </table>
+        <!-- / TABLE HEAD -->
+
+        <!-- TABLE CORPS -->
+        <table bgcolor="#2c3164" width="100%" border="0" cellpadding="0" cellspacing="0">
+            <tbody>
+            <tr>
+                <td height="30" style="font-size: 30px; line-height: 30px;">&nbsp;</td>
+            </tr>
+            <tr>
+                <td>
+                    <table align="center" width="590" border="0" cellpadding="0" cellspacing="0">
+                        <tbody>
+                        <tr>
+                            <td align="left" style="text-align: left; font-size: 13px; color: #989bb9; font-family: 'Product Sans',Helvetica, Arial, sans-serif">
+                                <p>Bonjour,</p>
+                                <p>Une demande de réinitialisations de mot de passe à été enregistré suivant les informations suivantes:</p><br>
+                                <ul>
+                                    <li>Identifiant: <strong><?= $email; ?></strong></li>
+                                    <li>ADRESSE IP: <strong><?= $_SERVER['REMOTE_ADDR']; ?></strong></li>
+                                    <li>Date de la demande: <strong><?= date("d-m-Y à H:i"); ?></strong></li>
+                                </ul>
+                                <p>
+                                    Si vous êtes celui qui à demander cette réinitialisation, veuillez cliquer sur le lien ci-dessous afin de réinitialiser le mot de passe:
+                                </p>
+                            </td>
+                        </tr>
+                        </tbody>
+                    </table>
+                </td>
+            </tr>
+            <tr>
+                <td height="30" style="font-size: 30px; line-height: 30px;">&nbsp;</td>
+            </tr>
+            <tr>
+                <td align="center" style="text-align: center;">
+                    <div><!--[if mso]>
+                        <v:roundrect xmlns:v="urn:schemas-microsoft-com:vml" xmlns:w="urn:schemas-microsoft-com:office:word" href="http://" style="height:80px;v-text-anchor:middle;width:300px;" arcsize="125%" strokecolor="#32f143" fillcolor="#32f143">
+                            <w:anchorlock/>
+                            <center style="color:#ffffff;font-family:sans-serif;font-size:13px;font-weight:bold;">Réinitialiser votre mot de passe</center>
+                        </v:roundrect>
+                        <![endif]-->
+                        <a href="<?= $constante->getUrl(array('core/'), false, false); ?>login.php?action=reset-password-2&token=<?= $token; ?>" style="background-color:#32f143;border:1px solid #32f143;border-radius:100px;color:#ffffff;display:inline-block;font-family:sans-serif;font-size:13px;font-weight:bold;line-height:80px;text-align:center;text-decoration:none;width:300px;-webkit-text-size-adjust:none;mso-hide:all;">Réinitialiser votre mot de passe</a>
+                    </div>
+                </td>
+            </tr>
+            <tr>
+                <td height="30" style="font-size: 30px; line-height: 30px;">&nbsp;</td>
+            </tr>
+            <tr>
+                <td>
+                    <table align="center" width="590" border="0" cellpadding="0" cellspacing="0">
+                        <tbody>
+                        <tr>
+                            <td align="left" style="text-align: left; font-size: 13px; color: #989bb9; font-family: 'Product Sans',Helvetica, Arial, sans-serif">
+                                <p>Si vous avez des question sur l'utilisation de nos services, n'hésitez pas à nous recontacter.</p>
+                                <p>Cordialement,</p><br>
+                                <p>LE SUPPORT TECHNIQUE<br><i>GAMESHOP</i></p>
+                            </td>
+                        </tr>
+                        </tbody>
+                    </table>
+                </td>
+            </tr>
+            <tr>
+                <td height="30" style="font-size: 30px; line-height: 30px;">&nbsp;</td>
+            </tr>
+            </tbody>
+        </table>
+        <!-- /TABLE CORPS -->
+
+        <!-- TABLE FOOTER -->
+        <table bgcolor="#131448" width="100%" border="0" cellpadding="0" cellspacing="0">
+            <tbody>
+            <tr>
+                <td background="http://imgur.com/kBStm0X" bgcolor="#373e80" valign="top">
+                    <!--[if gte mso 9]>
+                    <v:rect xmlns:v="urn:schemas-microsoft-com:vml" fill="true" stroke="false" style="mso-width-percent:1000;">
+                        <v:fill type="tile" src="http://imgur.com/kBStm0X" color="#373e80" />
+                        <v:textbox style="mso-fit-shape-to-text:true" inset="0,0,0,0">
+                    <![endif]-->
+                    <div>
+                        <table align="center" width="590" border="0" cellpadding="0" cellspacing="0">
+                            <tbody>
+                            <tr>
+                                <td height="30" style="font-size: 30px; line-height: 30px;">&nbsp;</td>
+                            </tr>
+                            <tr>
+                                <td align="left" style="text-align: left; font-size: 15px; color: white; font-family: 'Product Sans',Helvetica, Arial, sans-serif">
+                                    <img src="<?= $constante->getUrl(array('images/'), true, false); ?>logo.png" width="240" border="0" alt="Logo GAMESHOP" />
+                                </td>
+                            </tr>
+                            <tr>
+                                <td align="right" style="text-align: right; font-size: 15px; color: white; font-family: 'Product Sans',Helvetica, Arial, sans-serif">
+                                    <a href="" style="color: white; padding-right: 5px;"><i class="fa fa-globe"></i></a>
+                                    <a href="" style="color: white; padding-right: 5px;"><i class="fa fa-facebook"></i></a>
+                                    <a href="" style="color: white; padding-right: 5px;"><i class="fa fa-twitter"></i></a>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td height="30" style="font-size: 30px; line-height: 30px;">&nbsp;</td>
+                            </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </td>
+            </tr>
+            </tbody>
+        </table>
+        <!-- /TABLE FOOTER -->
+
+        </body>
+        </html>
+        <?php
+        $message = ob_get_clean();
+        $mail = mail($to, $sujet, $message, $headers);
+        if($mail === TRUE)
+        {
+            header("Location: ../index.php?view=login&sub=reset-password&success=reset-password-1");
+        }else{
+            header("Location: ../index.php?view=login&sub=reset-password&error=reset-password-1");
+        }
     }else{
         header("Location: ../index.php?view=login&sub=reset-password&error=correspondance");
     }
