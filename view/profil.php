7b2dada1f616f9c83a27a009c419f57b8eaa1e3d
CRIDIP-SWD <mmockelyn@cridip.com>
2015-12-29 18:14:05 +0100

    GAMESHOP UPDATE

--- view/profil.php
+++ view/profil.php
@@ -3,3 +3,20 @@ if($_SESSION['logged'] == false) {
     header("Location: index.php?view=login&warning=no-connect");
 }
 ?>
+<!-- Page Title
+		============================================= -->
+<section id="page-title">
+
+    <div class="container clearfix">
+        <h1>MON COMPTE</h1>
+        <span>Bienvenue</span>
+        <ol class="breadcrumb">
+            <li><a href="#">GAMESHOP</a></li>
+            <li><a href="#">MON COMPTE</a></li>
+            <li class="active">Bienvenue</li>
+        </ol>
+    </div>
+
+</section><!-- #page-title end -->
+
+
