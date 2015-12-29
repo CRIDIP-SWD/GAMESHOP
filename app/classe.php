91a027fc4bf5230e92bd9ef2e0703d5c1a92beca
CRIDIP-SWD <mmockelyn@cridip.com>
2015-12-29 17:57:22 +0100

    GAMESHOP UPDATE

--- app/classe.php
+++ app/classe.php
@@ -71,7 +71,7 @@ if(isset($_SESSION['logged'])){
      * STEAM CONNECTOR INIT
      */
     $steam = new \SteamApi\User('444446B16CB7611E5E74F4752A35EB5C', $info_client['pseudo_steam']);
-    var_dump($steam->GetPlayerSummaries()):
+    var_dump($steam->GetPlayerSummaries());
     die();
 }
 
