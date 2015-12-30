a8fcb7abca4920ed2b7f3933abf99890a01b93b9
CRIDIP-SWD <mmockelyn@cridip.com>
2015-12-30 03:55:08 +0100

    GAMESHOP UPDATE

--- app/classe.php
+++ app/classe.php
@@ -32,36 +32,39 @@ $client_cls = new client();
 if(isset($_SESSION['logged'])){
     $info_client = $client_cls->info_client($_SESSION['email']);
 
-    /*
- * PSN NETWORK API INIT
- */
-    $client_psn = new \Guzzle\Http\Client('', ['redirect.disable' => true]);
-    $connect_psn = new \Gumer\PSN\Http\Connection('FR', 'FR');
-    $connect_psn->setGuzzle($client_psn);
-    $provider_psn = new \Gumer\PSN\Authentication\UserProvider($connect_psn);
-    $auth_psn = \Gumer\PSN\Authentication\Manager::instance($provider_psn);
+    if(!empty($info_client['pseudo_psn']))
+    {
+                /*
+         * PSN NETWORK API INIT
+         */
+                $client_psn = new \Guzzle\Http\Client('', ['redirect.disable' => true]);
+                $connect_psn = new \Gumer\PSN\Http\Connection('FR', 'FR');
+                $connect_psn->setGuzzle($client_psn);
+                $provider_psn = new \Gumer\PSN\Authentication\UserProvider($connect_psn);
+                $auth_psn = \Gumer\PSN\Authentication\Manager::instance($provider_psn);
 
-    $auth_psn->attempt($info_client['pseudo_psn'], $info_client['pass_psn']);
-//$auth_psn->attempt('syltheron@gmail.com', '1992maxime');
+                $auth_psn->attempt($info_client['pseudo_psn'], $info_client['pass_psn']);
+        //$auth_psn->attempt('syltheron@gmail.com', '1992maxime');
 
-//Info PSN
-    $request_info = new \Gumer\PSN\Requests\GetMyInfoRequest();
-    $response_info = $connect_psn->call($request_info);
-    $info = json_decode($response_info->getBody(true), true);
-    //
+        //Info PSN
+                $request_info = new \Gumer\PSN\Requests\GetMyInfoRequest();
+                $response_info = $connect_psn->call($request_info);
+                $info = json_decode($response_info->getBody(true), true);
+                //
 
-//Profil
-    $request_profil = new \Gumer\PSN\Requests\ProfileRequest();
-    $request_profil->setUserId($info['onlineId']);
-    $response_profil = $connect_psn->call($request_profil);
-    $profil = json_decode($response_profil->getBody(true), true);
-    //var_dump($profil);
-//FriendList
-    $request_friend = new \Gumer\PSN\Requests\FriendsListRequest();
-    $request_friend->setUserId($info['onlineId']);
-    $response_friend = $connect_psn->call($request_friend);
-    $friend = json_decode($response_friend->getBody(true), true);
-    //var_dump($friend);
+        //Profil
+                $request_profil = new \Gumer\PSN\Requests\ProfileRequest();
+                $request_profil->setUserId($info['onlineId']);
+                $response_profil = $connect_psn->call($request_profil);
+                $profil = json_decode($response_profil->getBody(true), true);
+                //var_dump($profil);
+        //FriendList
+                $request_friend = new \Gumer\PSN\Requests\FriendsListRequest();
+                $request_friend->setUserId($info['onlineId']);
+                $response_friend = $connect_psn->call($request_friend);
+                $friend = json_decode($response_friend->getBody(true), true);
+                //var_dump($friend);
+    }
 
     /*
      * XBOX LIVE CONNECTOR INIT
