3cb96e7c95ec1e21f7070d0708b008c0c05c0144
CRIDIP-SWD <mmockelyn@cridip.com>
2015-12-30 02:34:06 +0100

    GAMESHOP UPDATE

--- cron/xboximport.php
+++ cron/xboximport.php
@@ -3,12 +3,12 @@
 include "../app/classe.php";
 $sql_client = mysql_query("SELECT * FROM client")or die(mysql_error());
 while($client = mysql_fetch_array($sql_client)){
-    var_dump($email = $clients['email']);
-    var_dump($nom_client = $clients['nom_client']);
-    var_dump($prenom_client = $clients['prenom_client']);
-    var_dump($pseudo_psn = $clients['pseudo_psn']);
-    var_dump($pass_psn = $clients['pass_psn']);
-    var_dump($pseudo_xbox = $clients['pseudo_xbox']);
-    var_dump($pseudo_steam = $clients['pseudo_steam']);
-    var_dump($point = $clients['point']);
+    var_dump($email = $client['email']);
+    var_dump($nom_client = $client['nom_client']);
+    var_dump($prenom_client = $client['prenom_client']);
+    var_dump($pseudo_psn = $client['pseudo_psn']);
+    var_dump($pass_psn = $client['pass_psn']);
+    var_dump($pseudo_xbox = $client['pseudo_xbox']);
+    var_dump($pseudo_steam = $client['pseudo_steam']);
+    var_dump($point = $client['point']);
 }
