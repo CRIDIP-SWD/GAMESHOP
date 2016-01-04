eb17179dca652e356d459445d4b71786944d5125
CRIDIP-SWD <mmockelyn@cridip.com>
2016-01-05 00:00:14 +0100

    GAMESHOP UPDATE

--- view/profil.php
+++ view/profil.php
@@ -419,33 +419,58 @@ if($_SESSION['logged'] == false) {
                         <div class="modal-content">
                             <div class="modal-header">
                                 <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-                                <h4 class="modal-title" id="myModalLabel">Modal Heading</h4>
+                                <h4 class="modal-title" id="myModalLabel"><i class="icon-edit-sign"></i> Modifier mes informations de compte</h4>
                             </div>
-                            <div class="modal-body">
-                                <h4>Text in a modal</h4>
-                                <p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula.</p>
-
-                                <h4>Popover in a modal</h4>
-                                <p>This <a href="#" role="button" class="btn btn-default popover-test" title="" data-content="And here's some amazing content. It's very engaging. right?" data-original-title="A Title">button</a> should trigger a popover on click.</p>
-
-                                <h4>Tooltips in a modal</h4>
-                                <p><a href="#" class="tooltip-test" title="" data-original-title="Tooltip">This link</a> and <a href="#" class="tooltip-test" title="" data-original-title="Tooltip">that link</a> should have tooltips on hover.</p>
-                                <hr>
-                                <h4>Overflowing text to show scroll behavior</h4>
-                                <p>Cras mattis consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.</p>
-                                <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor.</p>
-                                <p>Aenean lacinia bibendum nulla sed consectetur. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Donec sed odio dui. Donec ullamcorper nulla non metus auctor fringilla.</p>
-                                <p>Cras mattis consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.</p>
-                                <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor.</p>
-                                <p>Aenean lacinia bibendum nulla sed consectetur. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Donec sed odio dui. Donec ullamcorper nulla non metus auctor fringilla.</p>
-                                <p>Cras mattis consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.</p>
-                                <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor.</p>
-                                <p class="nobottommargin">Aenean lacinia bibendum nulla sed consectetur. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Donec sed odio dui. Donec ullamcorper nulla non metus auctor fringilla.</p>
-                            </div>
-                            <div class="modal-footer">
-                                <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-                                <button type="button" class="btn btn-primary">Save changes</button>
-                            </div>
+                            <form class="form-horizontal" action="<?= $constante->getUrl(array('core/'), false, false); ?>account.php" method="post">
+                                <div class="modal-body">
+                                    <div class="form-group">
+                                        <label class="control-label col-md-3" for="r">Nom:</label>
+                                        <div class="col-md-9">
+                                            <input id="r" type="text" name="nom_client" value="<?= $info_client['nom_client']; ?>" class="form-control" />
+                                        </div>
+                                    </div>
+                                    <div class="form-group">
+                                        <label class="control-label col-md-3" for="r">Prénom:</label>
+                                        <div class="col-md-9">
+                                            <input id="r" type="text" name="prenom_client" value="<?= $info_client['prenom_client']; ?>" class="form-control" />
+                                        </div>
+                                    </div>
+                                    <h2>Identification</h2>
+                                    <div class="form-group">
+                                        <label class="control-label col-md-3" for="r">ID Playstation Network:</label>
+                                        <div class="col-md-9">
+                                            <input id="r" type="text" name="pseudo_psn" value="<?= $info_client['pseudo_psn']; ?>" class="form-control" />
+                                            <span class="help-block">Adresse email de connexion au PSN</span>
+                                        </div>
+                                    </div>
+                                    <div class="form-group">
+                                        <label class="control-label col-md-3" for="r">Mot de Passe PSN:</label>
+                                        <div class="col-md-9">
+                                            <input id="r" type="password" name="pass_psn" value="<?= $info_client['pass_psn']; ?>" class="form-control" />
+                                        </div>
+                                    </div>
+                                    <hr>
+                                    <div class="form-group">
+                                        <label class="control-label col-md-3" for="r">XBOX LIVE Gamertag:</label>
+                                        <div class="col-md-9">
+                                            <input id="r" type="text" name="pseudo_xbox" value="<?= $info_client['pseudo_xbox']; ?>" class="form-control" />
+                                            <span class="help-block">Gamertag = Pseudo XBOX LIVE</span>
+                                        </div>
+                                    </div>
+                                    <hr>
+                                    <div class="form-group">
+                                        <label class="control-label col-md-3" for="r">ID STEAM:</label>
+                                        <div class="col-md-9">
+                                            <input id="r" type="text" name="pseudo_steam" value="<?= $info_client['pseudo_steam']; ?>" class="form-control" />
+                                            <span class="help-block">Identifiant 64 caractère</span>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="modal-footer">
+                                    <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                                    <button type="button" class="btn btn-primary">Save changes</button>
+                                </div>
+                            </form>
                         </div>
                     </div>
                 </div>
