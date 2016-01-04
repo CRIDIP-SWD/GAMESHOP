84298ed89234c92142c74b21d65d3bff36bb1d62
CRIDIP-SWD <mmockelyn@cridip.com>
2016-01-04 23:20:21 +0100

    GAMESHOP UPDATE

--- app/app.php
+++ app/app.php
@@ -39,8 +39,11 @@ class constante extends app{
     const NOM_SITE          = "Gameshop";
     const SOURCES           = "http://ns342142.ip-5-196-76.eu/sources/gameshop/";
     const MAINTENANCE       = 1;
-    const IP_MAIN           = array("109.190.224.161", "109.190.65.252");
-
+    const IP_MAIN           = "109.190.65.252";
+    /*
+     * ADRESSE BUREAU = 109.190.224.161
+     * ADRESSE MAISON = 109.190.65.252
+     */
     /**
      * @param $dos array Permet de parser sous forme string le tableau array=$dos
      * @return string retourne un format standard de link HTML
