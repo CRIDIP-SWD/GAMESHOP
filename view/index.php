b7107202613fbe5e169aeb28496eb2b70d057cba
CRIDIP-SWD <mmockelyn@cridip.com>
2016-01-05 18:02:04 +0100

    GAMESHOP UPDATE

--- view/index.php
+++ view/index.php
@@ -31,6 +31,9 @@
                     <div class="caption sft" data-x="-300" data-y="300" data-speed="700" data-start="3000" data-easing="easeOutLeft">
                         <img src="<?= $constante->getUrl('', false, true); ?>slides/annee/nathan.png" />
                     </div>
+                    <div class="caption sft" data-x="1000" data-y="0" data-speed="700" data-start="2500" data-easing="easeOutLeft">
+                        <img src="<?= $constante->getUrl('', false, true); ?>slides/annee/link.png" />
+                    </div>
                 </li>
             </ul>
 
