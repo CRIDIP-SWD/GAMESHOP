e1dd0a2ba677d9892069515d28cfa25836f7b349
CRIDIP-SWD <mmockelyn@cridip.com>
2016-01-06 00:28:22 +0100

    GAMESHOP UPDATE

--- view/profil.php
+++ view/profil.php
@@ -382,7 +382,7 @@ if($_SESSION['logged'] == false) {
                 </div>
             </div>
             <div class="contaner clearfix topmargin">
-                <div class="row">
+                <div class="row bottommargin-sm">
                     <div class="col-md-4" style="">
                         <div class="feature-box fbox-center fbox-effect" style="background-color: rgb(233, 233, 233); border: 1px none; box-shadow: 0px 0px 5px 1px rgba(0, 0, 0, 0.1) inset; border-radius: 5px; padding: 25px;">
                             <div class="fbox-icon">
