b618f2c1da05d37dd2d0f35e4b74fbfb6394aa34
CRIDIP-SWD <mmockelyn@cridip.com>
2016-01-06 14:16:18 +0100

    GAMESHOP UPDATE

--- view/profil.php
+++ view/profil.php
@@ -876,5 +876,5 @@ if($_SESSION['logged'] == false) {
 
 <!-- AUTRE JS -->
 <script type="text/javascript">
-    $("#telephone").maskInput('0033999999999');
+    $("#telephone").maskInput('0033.999999999');
 </script>
