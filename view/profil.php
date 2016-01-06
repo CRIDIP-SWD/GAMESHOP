778e945a1b2f480d707c3eadb5d7da9551d8800f
CRIDIP-SWD <mmockelyn@cridip.com>
2016-01-07 00:32:37 +0100

    GAMESHOP UPDATE

--- view/profil.php
+++ view/profil.php
@@ -893,7 +893,59 @@ if($_SESSION['logged'] == false) {
             <div class="container-fullwidth clearfix bottommargin-lg">
                 <div class="row">
                     <div class="col-md-3">
+                        <div class="widget clearfix">
 
+                            <h4>Nouveau produit</h4>
+                            <div id="post-list-footer">
+
+                                <div class="spost clearfix">
+                                    <div class="entry-image">
+                                        <a href="#"><img alt="Image" src="images/shop/small/1.jpg"></a>
+                                    </div>
+                                    <div class="entry-c">
+                                        <div class="entry-title">
+                                            <h4><a href="#">Blue Round-Neck Tshirt</a></h4>
+                                        </div>
+                                        <ul class="entry-meta">
+                                            <li class="color">$29.99</li>
+                                            <li><i class="icon-star3"></i> <i class="icon-star3"></i> <i class="icon-star3"></i> <i class="icon-star3"></i> <i class="icon-star-half-full"></i></li>
+                                        </ul>
+                                    </div>
+                                </div>
+
+                                <div class="spost clearfix">
+                                    <div class="entry-image">
+                                        <a href="#"><img alt="Image" src="images/shop/small/6.jpg"></a>
+                                    </div>
+                                    <div class="entry-c">
+                                        <div class="entry-title">
+                                            <h4><a href="#">Checked Short Dress</a></h4>
+                                        </div>
+                                        <ul class="entry-meta">
+                                            <li class="color">$23.99</li>
+                                            <li><i class="icon-star3"></i> <i class="icon-star3"></i> <i class="icon-star3"></i> <i class="icon-star-half-full"></i> <i class="icon-star-empty"></i></li>
+                                        </ul>
+                                    </div>
+                                </div>
+
+                                <div class="spost clearfix">
+                                    <div class="entry-image">
+                                        <a href="#"><img alt="Image" src="images/shop/small/7.jpg"></a>
+                                    </div>
+                                    <div class="entry-c">
+                                        <div class="entry-title">
+                                            <h4><a href="#">Light Blue Denim Dress</a></h4>
+                                        </div>
+                                        <ul class="entry-meta">
+                                            <li class="color">$19.99</li>
+                                            <li><i class="icon-star3"></i> <i class="icon-star3"></i> <i class="icon-star3"></i> <i class="icon-star-empty"></i> <i class="icon-star-empty"></i></li>
+                                        </ul>
+                                    </div>
+                                </div>
+
+                            </div>
+
+                        </div>
                     </div>
                     <div class="col-md-9">
                         <div class="container clearfix bottommargin">
