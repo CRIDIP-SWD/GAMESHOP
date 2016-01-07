2b52e5ee301fae3dec90c86d9f57ea2fa4eda7ef
CRIDIP-SWD <mmockelyn@cridip.com>
2016-01-07 01:43:35 +0100

    GAMESHOP UPDATE

--- view/profil.php
+++ view/profil.php
@@ -1090,7 +1090,7 @@ if($_SESSION['logged'] == false) {
                                                     </tr>
                                                     <tr>
                                                         <td style="width: 25%; padding-bottom: 10px; padding-top: 10px;">Nb d'article</td>
-                                                        <td style="width: 75%; padding-bottom: 10px; padding-top: 10px; font-weight: 700;"><?=  ?></td>
+                                                        <td style="width: 75%; padding-bottom: 10px; padding-top: 10px; font-weight: 700;"><?= $cmd_cls->count_article($num_commande); ?></td>
                                                     </tr>
                                                 </tbody>
                                             </table>
