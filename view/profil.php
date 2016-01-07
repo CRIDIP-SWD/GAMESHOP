abb286ad0359bbf07a6899d1bf93c05f6d2c8372
CRIDIP-SWD <mmockelyn@cridip.com>
2016-01-07 01:37:04 +0100

    GAMESHOP UPDATE

--- view/profil.php
+++ view/profil.php
@@ -1059,26 +1059,32 @@ if($_SESSION['logged'] == false) {
                         <div class="tabs tabs-bordered clearfix" id="tab-2">
 
                             <ul class="tab-nav clearfix">
-                                <li><a href="#tabs-5"><i class="icon-home2 norightmargin"></i></a></li>
-                                <li><a href="#tabs-6">Nunc tincidunt</a></li>
-                                <li><a href="#tabs-7">Proin dolor</a></li>
-                                <li class="hidden-phone"><a href="#tabs-8">Aenean lacinia</a></li>
+                                <li><a href="#general">GENERAL</a></li>
+                                <li><a href="#item">ARTICLES</a></li>
+                                <li><a href="#customers">CLIENT</a></li>
                             </ul>
 
                             <div class="tab-container">
 
-                                <div class="tab-content clearfix" id="tabs-5">
-                                    Proin elit arcu, rutrum commodo, vehicula tempus, commodo a, risus. Curabitur nec arcu. Donec sollicitudin mi sit amet mauris. Nam elementum quam ullamcorper ante. Etiam aliquet massa et lorem. Mauris dapibus lacus auctor risus. Aenean tempor ullamcorper leo. Vivamus sed magna quis ligula eleifend adipiscing. Duis orci. Aliquam sodales tortor vitae ipsum. Aliquam nulla. Duis aliquam molestie erat. Ut et mauris vel pede varius sollicitudin. Sed ut dolor nec orci tincidunt interdum. Phasellus ipsum. Nunc tristique tempus lectus.
+                                <div class="tab-content clearfix" id="general">
+                                    <h1>Vue d'ensemble</h1>
+                                    <div class="row">
+                                        <div class="col-md-6">
+                                            <h2>Informations Globales</h2>
+                                            <table style="width: 100%;">
+                                                <tbody>
+                                                    <tr>
+                                                        <td style="width: 25%;">Numéro de la commande</td>
+                                                        <td style="width: 75%; font-weight: 700;"><?= $num_commande; ?></td>
+                                                    </tr>
+                                                </tbody>
+                                            </table>
+                                        </div>
+                                    </div>
                                 </div>
-                                <div class="tab-content clearfix" id="tabs-6">
-                                    Morbi tincidunt, dui sit amet facilisis feugiat, odio metus gravida ante, ut pharetra massa metus id nunc. Duis scelerisque molestie turpis. Sed fringilla, massa eget luctus malesuada, metus eros molestie lectus, ut tempus eros massa ut dolor. Aenean aliquet fringilla sem. Suspendisse sed ligula in ligula suscipit aliquam. Praesent in eros vestibulum mi adipiscing adipiscing. Morbi facilisis. Curabitur ornare consequat nunc. Aenean vel metus. Ut posuere viverra nulla. Aliquam erat volutpat. Pellentesque convallis. Maecenas feugiat, tellus pellentesque pretium posuere, felis lorem euismod felis, eu ornare leo nisi vel felis. Mauris consectetur tortor et purus.
+                                <div class="tab-content clearfix" id="item">
                                 </div>
-                                <div class="tab-content clearfix" id="tabs-7">
-                                    <p>Mauris eleifend est et turpis. Duis id erat. Suspendisse potenti. Aliquam vulputate, pede vel vehicula accumsan, mi neque rutrum erat, eu congue orci lorem eget lorem. Vestibulum non ante. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Fusce sodales. Quisque eu urna vel enim commodo pellentesque. Praesent eu risus hendrerit ligula tempus pretium. Curabitur lorem enim, pretium nec, feugiat nec, luctus a, lacus.</p>
-                                    Duis cursus. Maecenas ligula eros, blandit nec, pharetra at, semper at, magna. Nullam ac lacus. Nulla facilisi. Praesent viverra justo vitae neque. Praesent blandit adipiscing velit. Suspendisse potenti. Donec mattis, pede vel pharetra blandit, magna ligula faucibus eros, id euismod lacus dolor eget odio. Nam scelerisque. Donec non libero sed nulla mattis commodo. Ut sagittis. Donec nisi lectus, feugiat porttitor, tempor ac, tempor vitae, pede. Aenean vehicula velit eu tellus interdum rutrum. Maecenas commodo. Pellentesque nec elit. Fusce in lacus. Vivamus a libero vitae lectus hendrerit hendrerit.
-                                </div>
-                                <div class="tab-content clearfix" id="tabs-8">
-                                    Praesent in eros vestibulum mi adipiscing adipiscing. Morbi facilisis. Curabitur ornare consequat nunc. Aenean vel metus. Ut posuere viverra nulla. Aliquam erat volutpat. Pellentesque convallis. Maecenas feugiat, tellus pellentesque pretium posuere, felis lorem euismod felis, eu ornare leo nisi vel felis. Mauris consectetur tortor et purus.
+                                <div class="tab-content clearfix" id="customers">
                                 </div>
 
                             </div>
