c19ca217b9c3e68db0aa11b1c2cfa9745f04371e
CRIDIP-SWD <mmockelyn@cridip.com>
2016-01-08 11:41:20 +0100

    GAMESHOP UPDATE

--- view/profil.php
+++ view/profil.php
@@ -1223,7 +1223,7 @@ if($_SESSION['logged'] == false) {
                                                     ?>
                                                         <tr>
                                                             <td class="text-center"><?= $article['id']; ?></td>
-                                                            <td class="text-center"><a href="index.php?view=produit&ref_preoduit=<?= $article['ref_produit']; ?>"<?= $article['designation']; ?></td>
+                                                            <td class="text-center"><a href="index.php?view=produit&ref_preoduit=<?= $article['ref_produit']; ?>"><?= $article['designation']; ?></a></td>
                                                             <td class="text-center"><?= $article['ref_produit']; ?></td>
                                                             <td class="text-center"><?= $categorie['designation_cat']; ?> / <?= $subcategorie['designation_subcat']; ?></td>
                                                             <td class="text-center"><?= number_format($article['prix_vente'], 2, ',', ' ')." €"; ?></td>
