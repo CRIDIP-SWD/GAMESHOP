197fbcd427511a00b022cd20d9b6949e93f0d148
CRIDIP-SWD <mmockelyn@cridip.com>
2016-01-08 17:11:39 +0100

    GAMESHOP UPDATE

--- view/default.php
+++ view/default.php
@@ -275,7 +275,7 @@ ini_set('display_errors', 1);
                     <!-- Top Cart
                     ============================================= -->
                     <div id="top-cart">
-                        <a href="#" id="top-cart-trigger"><i class="icon-shopping-cart"></i><span>5</span></a>
+                        <a href="#" id="top-cart-trigger"><i class="icon-shopping-cart"></i><span><?= $panier_cls->compterArticles(); ?></span></a>
                         <div class="top-cart-content">
                             <div class="top-cart-title">
                                 <h4>Votre Panier</h4>
@@ -291,20 +291,10 @@ ini_set('display_errors', 1);
                                         <span class="top-cart-item-quantity">x 2</span>
                                     </div>
                                 </div>
-                                <div class="top-cart-item clearfix">
-                                    <div class="top-cart-item-image">
-                                        <a href="#"><img src="images/shop/small/6.jpg" alt="Light Blue Denim Dress" /></a>
-                                    </div>
-                                    <div class="top-cart-item-desc">
-                                        <a href="#">Light Blue Denim Dress</a>
-                                        <span class="top-cart-item-price">$24.99</span>
-                                        <span class="top-cart-item-quantity">x 3</span>
-                                    </div>
-                                </div>
                             </div>
                             <div class="top-cart-action clearfix">
                                 <span class="fleft top-checkout-price">$114.95</span>
-                                <button class="button button-3d button-small nomargin fright">View Cart</button>
+                                <button class="button button-3d button-small nomargin fright" onclick="window.location.href='index.php?view=panier'">Voir mon panier</button>
                             </div>
                         </div>
                     </div><!-- #top-cart end -->
