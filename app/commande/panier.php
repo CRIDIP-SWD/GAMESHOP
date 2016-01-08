56c6adb01e3cba9e9d0997d232657fdce776d4f8
CRIDIP-SWD <mmockelyn@cridip.com>
2016-01-08 16:39:33 +0100

    GAMESHOP UPDATE

--- app/commande/panier.php
+++ app/commande/panier.php
@@ -13,9 +13,7 @@ class panier
 {
     public function __construct()
     {
-        if(!isset($_SESSION))
-        {
-            session_start();
-        }
+        if(!isset($_SESSION)) {session_start();}
+        if(!isset($_SESSION['panier'])){$_SESSION['panier'] = array();}
     }
 }
