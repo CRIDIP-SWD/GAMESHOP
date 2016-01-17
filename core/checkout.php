0c81e149e9ee65bdd3caeeeaa2042e907fa21d6c
CRIDIP-SWD <mmockelyn@cridip.com>
2016-01-17 04:21:31 +0100

    GAMESHOP UPDATE

--- core/checkout.php
+++ core/checkout.php
@@ -53,7 +53,7 @@ if(isset($_GET['action']) && $_GET['action'] == 'adresse')
     $sql = $DB->execute("INSERT INTO commande(idcommande, num_commande, date_commande, idclient, total_commande, date_livraison, destination, statut, adresse_fact, adresse_liv, methode_livraison, methode_paiement, prix_envoie)
                           VALUES (NULL, :num_commande, :date_commande, :idclient, :total_commande, :date_livraison, :destination, :statut, :adresse_fact, :adresse_liv, :methode_livraison, :methode_paiement, :prix_envoie)", $data);
 
-    foreach($_SESSION['panier'] as $article)
+    foreach($_SESSION['panier']['refProduit'] as $article)
     {
         var_dump($article);
     }
