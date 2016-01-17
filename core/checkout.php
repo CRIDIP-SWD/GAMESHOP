da4a899c011b81c843c97df08449dc2c713c412e
CRIDIP-SWD <mmockelyn@cridip.com>
2016-01-17 04:21:20 +0100

    GAMESHOP UPDATE

--- core/checkout.php
+++ core/checkout.php
@@ -55,7 +55,7 @@ if(isset($_GET['action']) && $_GET['action'] == 'adresse')
 
     foreach($_SESSION['panier'] as $article)
     {
-        var_dump($article[0]);
+        var_dump($article);
     }
 
     /*$error = "Impossible de Créer votre commande.<br>Veuillez contactez un administrateur.";
