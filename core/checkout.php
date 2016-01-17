3c9b41187028fd194e0ce88f92cfce69215a2324
CRIDIP-SWD <mmockelyn@cridip.com>
2016-01-17 04:11:23 +0100

    GAMESHOP UPDATE

--- core/checkout.php
+++ core/checkout.php
@@ -55,7 +55,7 @@ if(isset($_GET['action']) && $_GET['action'] == 'adresse')
 
     foreach($_SESSION['panier'] as $key => $article)
     {
-        $panier = array($article[$key]);
+        $panier = array($article);
         var_dump($panier);
     }
 
