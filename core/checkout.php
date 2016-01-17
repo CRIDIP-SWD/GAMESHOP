5b1a7bdad06b294cb9a90f507c28e40fdd5d2fb4
CRIDIP-SWD <mmockelyn@cridip.com>
2016-01-17 03:28:52 +0100

    GAMESHOP UPDATE

--- core/checkout.php
+++ core/checkout.php
@@ -48,12 +48,35 @@ if(isset($_GET['action']) && $_GET['action'] == 'adresse')
     );
 
     $num_commande = $data[0]->num_commande;
+    $nbArticles = count($_SESSION['panier']['refProduit']);
 
     $sql = $DB->execute("INSERT INTO commande(idcommande, num_commande, date_commande, idclient, total_commande, date_livraison, destination, statut, adresse_fact, adresse_liv, methode_livraison, methode_paiement, prix_envoie)
                           VALUES (NULL, :num_commande, :date_commande, :idclient, :total_commande, :date_livraison, :destination, :statut, :adresse_fact, :adresse_liv, :methode_livraison, :methode_paiement, :prix_envoie)", $data);
 
+    for($i=0; $i<$nbArticles;$i++):
+
+        $ref_produit = $_SESSION['panier']['refProduit'];
+        $qte = $_SESSION['panier']['qteProduit'];
+        $prix_article = $_SESSION['panier']['prixProduit'];
+
+        $produit = $DB->query("SELECT * FROM produits WHERE ref_produit = :ref_produit", array(
+            "ref_produit"           => $ref_produit
+        ));
+
+        $total_article_commande = $produit[0]->prix_vente * $qte;
+
+        $sql_article = $DB->execute("INSERT INTO commande_article(idcommandearticle, num_commande, ref_produit, qte, total_article_commande)
+                                        VALUES (NULL, :num_commande, :ref_produit, :qte, :total_article_commande)", array(
+            "num_commande"              => $num_commande,
+            "ref_produit"               => $ref_produit,
+            "qte"                       => $qte,
+            "total_article_commande"    => $total_article_commande
+        ));
+
+    endfor;
+
     $error = "Impossible de Créer votre commande.<br>Veuillez contactez un administrateur.";
-    if($sql == 1)
+    if($sql == 1 AND $sql_article >= 1)
     {
         header("Location: ../index.php?view=checkout&sub=adresse&num_commande=$num_commande");
     }else{
