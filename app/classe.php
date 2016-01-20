981cc3fb08c05d76c36759371a4892346f612603
CRIDIP-SWD <mmockelyn@cridip.com>
2016-01-20 19:11:39 +0100

    GAMESHOP UPDATE

--- app/classe.php
+++ app/classe.php
@@ -104,7 +104,7 @@ if(isset($_SESSION['logged'])){
     if(!empty($info_client[0]->pseudo_xbox))
     {
         $xbox = new xbox($info_client[0]->pseudo_xbox, 'fr-FR');
-        $xbox_statut = $xbox->call('test');
+        $xbox_statut = $xbox->call('statut');
         var_dump($xbox_statut);
     }
 }
