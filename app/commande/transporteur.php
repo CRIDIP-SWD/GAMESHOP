1bde1b70170956aaeec6c69c226e6f3cb465d43a
CRIDIP-SWD <mmockelyn@cridip.com>
2016-01-21 21:02:13 +0100

    GAMESHOP UPDATE

---
+++ app/commande/transporteur.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Maxime
+ * Date: 21/01/2016
+ * Time: 20:58
+ */
+
+namespace App\commande;
+
+
+class transporteur
+{
+
+}
---
+++ assets/data/transporteur.php
@@ -0,0 +1,16 @@
+<?php
+/*
+ * TABLEAU DES TARIFS DE LIVRAISON
+ */
+$data = array(
+    "La Poste" => array(
+        "0"         => 0,
+        "0.5"       => 2.30,
+        "1.00"      => 3.25,
+        "2.00"      => 4.85,
+        "5.00"      => 8.45,
+        "10.00"     => 15.98
+    )
+);
+
+var_dump($data);
