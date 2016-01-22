8810e849cb59a46bdd6ef99951401c249dd7056f
CRIDIP-SWD <mmockelyn@cridip.com>
2016-01-23 00:24:45 +0100

    GAMESHOP UPDATE

--- core/checkout.php
+++ core/checkout.php
@@ -184,8 +184,6 @@ if(isset($_POST['action']) && $_POST['action'] == 'process-paiement')
         $response = $paypal->request('SetExpressCheckout', $params);
         if($response)
         {
-            var_dump($response);
-            die();
             header("Location: https://www.sandbox.paypal.com/webscr?cmd=_express-checkout&useraction=commit&token=".$response['TOKEN']);
         }else{
             var_dump($paypal->errors);
