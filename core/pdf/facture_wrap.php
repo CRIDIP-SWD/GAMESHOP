f34137a974203dd207e27a391f43ae49333f4e7d
CRIDIP-SWD <mmockelyn@cridip.com>
2016-01-24 18:54:54 +0100

    GAMESHOP UPDATE

--- core/pdf/facture_wrap.php
+++ core/pdf/facture_wrap.php
@@ -8,7 +8,7 @@
             <div class="row">
                 <div class="col-md-2">LOGO</div>
                 <div class="col-md-8">FACTURE N° 52221452</div>
-                <div class="col-md-2">Page 1</div>
+                <div class="col-md-2">Page 1 <i class="icon-check-sign"></i></div>
             </div>
         </div>
     </body>
