96d62060ba90e8e4e054503cf31138f6639b2855
CRIDIP-SWD <mmockelyn@cridip.com>
2016-01-24 02:06:14 +0100

    GAMESHOP UPDATE

--- app/general/produit.php
+++ app/general/produit.php
@@ -123,7 +123,7 @@ class produit extends DB
         ));
         $point = $pointClient[0]->point;
         $revenuePoint = $this->revenue_point_total($num_commande);
-        var_dump($revenuePoint, $point);
+        var_dump($point);
         $revenue = $revenuePoint + $point;
         return $revenue;
     }
