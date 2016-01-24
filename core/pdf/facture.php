85565d603faedc2ba5e0dfc14fd109d4c3acca01
CRIDIP-SWD <mmockelyn@cridip.com>
2016-01-24 19:05:55 +0100

    GAMESHOP UPDATE

--- core/pdf/facture.php
+++ core/pdf/facture.php
@@ -8,13 +8,7 @@ require "facture_wrap.php";
 $content = ob_get_clean();
 
 $pdf = new Pdf(array(
-    'no-outline',         // Make Chrome not complain
-    'margin-top'    => 0,
-    'margin-right'  => 0,
-    'margin-bottom' => 0,
-    'margin-left'   => 0,
 
-    // Default page options
     'disable-smart-shrinking',
     'user-style-sheet' => 'pdf.css',
 ));
