27e109420fa50e7463a5afcbf1bc76e4ac0501b6
CRIDIP-SWD <mmockelyn@cridip.com>
2016-01-25 10:26:39 +0100

    GAMESHOP UPDATE

--- core/pdf/facture_wrap.php
+++ core/pdf/facture_wrap.php
@@ -11,38 +11,11 @@ $cmd = $DB->query("SELECT * FROM commande, client WHERE commande.idclient = clie
         <meta charset="UTF-8" />
     </head>
     <body>
-        <div id="header">
-            <div class="header_left">
-                <div class="logo">
-                    <img src="<?= $constante->getUrl(array('images/'), true, false); ?>logo.png" width="80"/>
-                </div>
-            </div>
-            <div class="header_right">
-                FACTURE
-            </div>
-        </div>
-        <div id="block_adresse">
-            <div class="adresse_societe">
-                <h2>GAMESHOP</h2>
-                20 Avenue Jean Jaures<br>
-                85100 Les Sables d'Olonne<br>
-                Tel: 06 33 13 43 30<br>
-                Mail: gamedistri@gmail.com
-            </div>
-            <div class="adresse_client">
-                <strong><?= $cmd[0]->nom_client; ?> <?= $cmd[0]->prenom_client; ?></strong><br>
-                <?= html_entity_decode($cmd[0]->adresse_liv); ?>
-            </div>
-        </div>
-        <div id="info_facture">
-            <table>
-                <tbody>
-                    <tr>
-                        <td>N° de Commande:</td>
-                        <td><?= $num_commande; ?></td>
-                    </tr>
-                </tbody>
-            </table>
-        </div>
+        <table style="width: 100%;">
+            <tr>
+                <td></td>
+                <td style="text-align: right">FACTURE</td>
+            </tr>
+        </table>
     </body>
 </html>
