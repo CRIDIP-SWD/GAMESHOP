a48e7846a46ad638673de8abc8b01a5ce86005c9
CRIDIP-SWD <mmockelyn@cridip.com>
2016-01-25 15:03:03 +0100

    GAMESHOP UPDATE

--- core/pdf/facture.php
+++ core/pdf/facture.php
@@ -12,5 +12,6 @@ $pdf = new Pdf(array(
     'disable-smart-shrinking',
     'user-style-sheet' => 'pdf.css',
 ));
+echo html_entity_decode($content);
 $pdf->addPage($content);
-$pdf->send();
+//$pdf->send();
