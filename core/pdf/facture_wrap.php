86e5fcf834290c5f26dbf43bf41f9baffa4bdda9
CRIDIP-SWD <mmockelyn@cridip.com>
2016-01-25 10:13:35 +0100

    GAMESHOP UPDATE

--- core/pdf/facture_wrap.php
+++ core/pdf/facture_wrap.php
@@ -1,3 +1,10 @@
+<?php
+$num_commande = $_GET['num_commande'];
+$cmd = $DB->query("SELECT * FROM commande, client WHERE commande.idclient = client.idclient AND num_commande = :num_commande", array(
+    "num_commande"  => $num_commande
+));
+
+?>
 <!DOCTYPE html>
 <html>
     <head>
@@ -8,15 +15,23 @@
             <div class="header_left">
                 <div class="logo">
                     <img src="<?= $constante->getUrl(array('images/'), true, false); ?>logo.png" width="80"/>
-                    <h2>GAMESHOP</h2>
-                    20 Avenue Jean Jaures<br>
-                    85100 Les Sables d'Olonne<br>
-                    Tel: 06 33 13 43 30<br>
-                    Mail: gamedistri@gmail.com
                 </div>
             </div>
             <div class="header_right">
                 FACTURE
+            </div>
+        </div>
+        <div id="block_adresse">
+            <div class="adresse_societe">
+                <h2>GAMESHOP</h2>
+                20 Avenue Jean Jaures<br>
+                85100 Les Sables d'Olonne<br>
+                Tel: 06 33 13 43 30<br>
+                Mail: gamedistri@gmail.com
+            </div>
+            <div class="adresse_client">
+                <strong><?= $cmd[0]->nom_client; ?> <?= $cmd[0]->prenom_client; ?></strong><br>
+                <?= $cmd[0]->adresse_liv; ?>
             </div>
         </div>
     </body>
