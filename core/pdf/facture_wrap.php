e105249486f3f719a7b64bf0f66dcb713aa313aa
CRIDIP-SWD <mmockelyn@cridip.com>
2016-01-25 10:04:08 +0100

    GAMESHOP UPDATE

--- core/pdf/facture_wrap.php
+++ core/pdf/facture_wrap.php
@@ -8,6 +8,9 @@
             <div class="header_left">
                 <div class="logo">
                     <img src="<?= $constante->getUrl(array('images/'), true, false); ?>logo.png" width="80"/>
+                    <h2>GAMESHOP</h2>
+                    20 Avenue Jean Jaures<br>
+                    85100 Les Sables d'Olonne
                 </div>
             </div>
             <div class="header_right">
