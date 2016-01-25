2318a684080a8aa5dcfaa61b69583d9d1f4eafb1
CRIDIP-SWD <mmockelyn@cridip.com>
2016-01-25 10:03:15 +0100

    GAMESHOP UPDATE

--- core/pdf/facture_wrap.php
+++ core/pdf/facture_wrap.php
@@ -7,7 +7,7 @@
         <div id="header">
             <div class="header_left">
                 <div class="logo">
-                    <img src="<?= $constante->getUrl(array('images/'), true, false); ?>logo.png" />
+                    <img src="<?= $constante->getUrl(array('images/'), true, false); ?>logo.png" width="80"/>
                 </div>
             </div>
             <div class="header_right">
