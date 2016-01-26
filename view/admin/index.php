e3bd6a76688e219e0f266ca5f3ee3f89b24e7b22
CRIDIP-SWD <mmockelyn@cridip.com>
2016-01-26 01:14:37 +0100

    GAMESHOP UPDATE

--- view/admin/index.php
+++ view/admin/index.php
@@ -409,6 +409,28 @@
                 <div class="col-md-3 well">
                     <button type="button" class="btn btn-primary btn-block" data-toggle="modal" data-target="#add-client"><i class="fa fa-user-plus"></i> Ajouter un client</button>
                 </div>
+                <div id="add-client" class="modal-block mfp-hide">
+                    <section class="panel">
+                        <header class="panel-heading">
+                            <h2 class="panel-title">Are you sure?</h2>
+                        </header>
+                        <div class="panel-body">
+                            <div class="modal-wrapper">
+                                <div class="modal-text">
+                                    <p>Are you sure that you want to delete this image?</p>
+                                </div>
+                            </div>
+                        </div>
+                        <footer class="panel-footer">
+                            <div class="row">
+                                <div class="col-md-12 text-right">
+                                    <button class="btn btn-primary modal-confirm">Confirm</button>
+                                    <button class="btn btn-default modal-dismiss">Cancel</button>
+                                </div>
+                            </div>
+                        </footer>
+                    </section>
+                </div>
             </div>
             <!-- end: page -->
         </section>
