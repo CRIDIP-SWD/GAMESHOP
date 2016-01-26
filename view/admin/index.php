92c80c2d50e55683f39e0bc36754aff97d95f8fd
CRIDIP-SWD <mmockelyn@cridip.com>
2016-01-26 17:47:48 +0100

    GAMESHOP UPDATE

--- view/admin/index.php
+++ view/admin/index.php
@@ -497,7 +497,7 @@
                                 <div class="profile-info">
                                     <h4 class="name text-weight-semibold"><?= $client[0]->nom_client; ?> <?= $client[0]->prenom_client; ?></h4>
                                     <h5 class="role"><?= $client[0]->email; ?></h5>
-                                    <div class="profile-footer">
+                                    <div class="profile-footer" style="display: inline;">
                                         <?php if($client[0]->pseudo_psn): ?>
                                             <img src="<?= $constante->getUrl(array(''), false, true); ?>autre/icone/psn.png" width="20" class="img-responsive" />
                                         <?php endif; ?>
