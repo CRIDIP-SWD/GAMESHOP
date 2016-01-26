98b99a351e297347c426c4ecbcf102ccbbac9bf0
CRIDIP-SWD <mmockelyn@cridip.com>
2016-01-26 01:21:00 +0100

    GAMESHOP UPDATE

--- view/admin/index.php
+++ view/admin/index.php
@@ -409,26 +409,31 @@
                 <div class="col-md-3 well">
                     <button type="button" class="mb-xs mt-xs mr-xs modal-basic btn btn-primary btn-block" href="#add-client"><i class="fa fa-user-plus"></i> Ajouter un client</button>
                 </div>
-                <div id="add-client" class="modal-block mfp-hide">
+                <div id="add-client" class="modal-block modal-block-lg modal-header-color modal-block-primary mfp-hide">
                     <section class="panel">
                         <header class="panel-heading">
-                            <h2 class="panel-title">Are you sure?</h2>
+                            <h2 class="panel-title">Nouveau Client</h2>
                         </header>
-                        <div class="panel-body">
-                            <div class="modal-wrapper">
-                                <div class="modal-text">
-                                    <p>Are you sure that you want to delete this image?</p>
+                        <form class="form-horizontal" action="core/account.php" method="post">
+                            <div class="panel-body">
+                                <div class="modal-wrapper">
+                                    <div class="form-group">
+                                        <label class="col-md-3 control-label">Adresse Mail</label>
+                                        <div class="col-md-9">
+                                            <input type="text" name="email" class="form-control">
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                        <footer class="panel-footer">
-                            <div class="row">
-                                <div class="col-md-12 text-right">
-                                    <button class="btn btn-primary modal-confirm">Confirm</button>
-                                    <button class="btn btn-default modal-dismiss">Cancel</button>
+                            <footer class="panel-footer">
+                                <div class="row">
+                                    <div class="col-md-12 text-right">
+                                        <button class="btn btn-primary modal-confirm">Confirm</button>
+                                        <button class="btn btn-default modal-dismiss">Cancel</button>
+                                    </div>
                                 </div>
-                            </div>
-                        </footer>
+                            </footer>
+                        </form>
                     </section>
                 </div>
             </div>
