1ddfc39cdaa175a1ca1b21d72c259e45318e1842
CRIDIP-SWD <mmockelyn@cridip.com>
2016-01-27 00:02:21 +0100

    GAMESHOP SQUAD

--- view/admin/index.php
+++ view/admin/index.php
@@ -521,8 +521,8 @@
                         <div class="panel-body">
                             <div class="row">
                                 <div class="col-md-4">
-                                    <div class="h4 text-weight-bold mb-none mt-lg">0</div>
-                                    <p class="text-xs text-muted mb-none">Commandes</p>
+                                    <div class="h2 text-weight-bold mb-none mt-lg">0</div>
+                                    <p class="text-muted mb-none">Commandes</p>
                                 </div>
                             </div>
                         </div>
