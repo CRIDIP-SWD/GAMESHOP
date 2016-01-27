8ce3418e9306fad89db7429a9d11b4e0e346b08c
CRIDIP-SWD <mmockelyn@cridip.com>
2016-01-27 10:28:58 +0100

    GAMESHOP UPDATE

--- assets/porto/javascripts/ajax.js
+++ assets/porto/javascripts/ajax.js
@@ -1,19 +1,21 @@
 /**
  * Created by SWD on 27/01/2016.
  */
-$('.table').on('click', 'btn-success', function(e){
-    e.preventDefault();
-    var $a = $(this);
-    var url = $a.attr('href');
+(function($){
+    $('.table').on('click', 'btn-success', function(e){
+        e.preventDefault();
+        var $a = $(this);
+        var url = $a.attr('href');
 
-    $.ajax(url)
-        .done(function(data, text, jqxhr){
-            alert(jqxhr.responseText);
-        })
-        .fail(function(jqxhr){
-            alert(jqxhr.responseText);
-        })
-        .always(function(){
-            $.a.text("<i class='fa fa-star fa-2x'></i>");
-        })
-})
+        $.ajax(url)
+            .done(function(data, text, jqxhr){
+                alert(jqxhr.responseText);
+            })
+            .fail(function(jqxhr){
+                alert(jqxhr.responseText);
+            })
+            .always(function(){
+                $.a.text("<i class='fa fa-star fa-2x'></i>");
+            });
+    });
+})(jQuery);
