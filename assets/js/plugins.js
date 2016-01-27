66d70872a7e92f9d7270a9be919d42f94c933bc3
CRIDIP-SWD <mmockelyn@cridip.com>
2016-01-27 10:24:54 +0100

    GAMESHOP UPDATE

--- assets/js/plugins.js
+++ assets/js/plugins.js
@@ -1250,20 +1250,3 @@ $(document).ready(function(){
 
 	});
 });
-
-$('.table').on('click', 'btn-success', function(e){
-	e.preventDefault();
-	var $a = $(this);
-	var url = $a.attr('href');
-
-	$.ajax(url)
-		.done(function(data, text, jqxhr){
-			alert(jqxhr.responseText);
-		})
-		.fail(function(jqxhr){
-			alert(jqxhr.responseText);
-		})
-		.always(function(){
-			$.a.text("<i class='fa fa-star fa-2x'></i>");
-		})
-})
