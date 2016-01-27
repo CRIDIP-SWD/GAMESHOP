4aada49a4ed458e974a96bf2e95d479a9f64e1f4
CRIDIP-SWD <mmockelyn@cridip.com>
2016-01-27 18:49:40 +0100

    GAMESHOP UPDATE

--- view/admin/index.php
+++ view/admin/index.php
@@ -859,69 +859,17 @@
                                 </div>
 
                                 <div class="form-group">
-                                    <label class="col-md-3 control-label">Basic select</label>
-                                    <div class="col-md-6">
-                                        <select data-plugin-selectTwo class="form-control populate">
-                                            <optgroup label="Alaskan/Hawaiian Time Zone">
-                                                <option value="AK">Alaska</option>
-                                                <option value="HI">Hawaii</option>
-                                            </optgroup>
-                                            <optgroup label="Pacific Time Zone">
-                                                <option value="CA">California</option>
-                                                <option value="NV">Nevada</option>
-                                                <option value="OR">Oregon</option>
-                                                <option value="WA">Washington</option>
-                                            </optgroup>
-                                            <optgroup label="Mountain Time Zone">
-                                                <option value="AZ">Arizona</option>
-                                                <option value="CO">Colorado</option>
-                                                <option value="ID">Idaho</option>
-                                                <option value="MT">Montana</option>
-                                                <option value="NE">Nebraska</option>
-                                                <option value="NM">New Mexico</option>
-                                                <option value="ND">North Dakota</option>
-                                                <option value="UT">Utah</option>
-                                                <option value="WY">Wyoming</option>
-                                            </optgroup>
-                                            <optgroup label="Central Time Zone">
-                                                <option value="AL">Alabama</option>
-                                                <option value="AR">Arkansas</option>
-                                                <option value="IL">Illinois</option>
-                                                <option value="IA">Iowa</option>
-                                                <option value="KS">Kansas</option>
-                                                <option value="KY">Kentucky</option>
-                                                <option value="LA">Louisiana</option>
-                                                <option value="MN">Minnesota</option>
-                                                <option value="MS">Mississippi</option>
-                                                <option value="MO">Missouri</option>
-                                                <option value="OK">Oklahoma</option>
-                                                <option value="SD">South Dakota</option>
-                                                <option value="TX">Texas</option>
-                                                <option value="TN">Tennessee</option>
-                                                <option value="WI">Wisconsin</option>
-                                            </optgroup>
-                                            <optgroup label="Eastern Time Zone">
-                                                <option value="CT">Connecticut</option>
-                                                <option value="DE">Delaware</option>
-                                                <option value="FL">Florida</option>
-                                                <option value="GA">Georgia</option>
-                                                <option value="IN">Indiana</option>
-                                                <option value="ME">Maine</option>
-                                                <option value="MD">Maryland</option>
-                                                <option value="MA">Massachusetts</option>
-                                                <option value="MI">Michigan</option>
-                                                <option value="NH">New Hampshire</option>
-                                                <option value="NJ">New Jersey</option>
-                                                <option value="NY">New York</option>
-                                                <option value="NC">North Carolina</option>
-                                                <option value="OH">Ohio</option>
-                                                <option value="PA">Pennsylvania</option>
-                                                <option value="RI">Rhode Island</option>
-                                                <option value="SC">South Carolina</option>
-                                                <option value="VT">Vermont</option>
-                                                <option value="VA">Virginia</option>
-                                                <option value="WV">West Virginia</option>
-                                            </optgroup>
+                                    <label class="col-md-3 control-label">Adresse de Facturation</label>
+                                    <div class="col-md-9">
+                                        <select data-plugin-selectTwo class="form-control populate" name="adresse_fact">
+                                            <?php
+                                            $sql_addresse_fact = $DB->query("SELECT * FROM client_adresse_fact WHERE idclient = :idclient", array(
+                                                "idclient"      => $idclient
+                                            ));
+                                            foreach($sql_addresse_fact as $adresse):
+                                            ?>
+                                            <option value="<?= $adresse->adresse; ?><br><?= $adresse->code_postal; ?> <?= $adresse->ville; ?>"><?= $adresse->alias; ?></option>
+                                            <?php endforeach; ?>
                                         </select>
                                     </div>
                                 </div>
