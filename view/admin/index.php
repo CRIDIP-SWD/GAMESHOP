4aa4e12d505ec59738aac3de1e78c5870e6c32e7
CRIDIP-SWD <mmockelyn@cridip.com>
2016-01-28 00:28:57 +0100

    GAMESHOP SQUAD

--- view/admin/index.php
+++ view/admin/index.php
@@ -907,3 +907,37 @@
         </section>
     <?php endif; ?>
 <?php endif; ?>
+<?php if(isset($_GET['sub']) && $_GET['sub'] == 'categories'): ?>
+    <?php if(!isset($_GET['data'])): ?>
+        <section role="main" class="content-body">
+            <header class="page-header">
+                <h2><i class="fa fa-search"></i> CATEGORIES</h2>
+
+                <div class="right-wrapper pull-right">
+                    <ol class="breadcrumbs">
+                        <li>
+                            <a href="index.php?view=admin_sha">
+                                <i class="fa fa-home"></i>
+                            </a>
+                        </li>
+                        <li><span>Catégorie</span></li>
+                    </ol>
+
+                    <a class="sidebar-right-toggle" data-open="sidebar-right"><i class="fa fa-chevron-left"></i></a>
+                </div>
+            </header>
+
+            <!-- start: page -->
+            <div class="row">
+                <section class="panel panel-primary">
+                    <header class="panel-heading">
+                        <div class="panel-actions">
+                            <a href="" class="panel-action"><i class="fa fa-arrow-circle-up"></i></a>
+                        </div>
+                    </header>
+                </section>
+            </div>
+            <!-- end: page -->
+        </section>
+    <?php endif; ?>
+<?php endif; ?>
