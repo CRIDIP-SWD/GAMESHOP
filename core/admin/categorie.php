11a13bbda74c24de6bcb162c62a2845e9ed8e16a
CRIDIP-SWD <mmockelyn@cridip.com>
2016-01-28 00:54:22 +0100

    GAMESHOP SQUAD

--- core/admin/categorie.php
+++ core/admin/categorie.php
@@ -1,5 +1,5 @@
 <?php
-if(isset($_POST['action']) && $_POST['action'] == 'add-categorie')
+if(isset($_POST['action']) && $_POST['action'] == 'add-categories')
 {
     require "../../app/classe.php";
     if(isset($_FILES['images_cat']) && $_FILES['images_cat']['error'] == 0)
