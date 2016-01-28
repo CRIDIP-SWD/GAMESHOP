3fa15f1de2171a644555af6edd19e25e4f1b5e51
CRIDIP-SWD <mmockelyn@cridip.com>
2016-01-28 10:08:57 +0100

    GAMESHOP UPDATE

--- core/admin/categorie.php
+++ core/admin/categorie.php
@@ -25,13 +25,6 @@ if(isset($_POST['action']) && $_POST['action'] == 'add-categories')
             }
 
         }
-    }else{
-        if($_FILES['images_cat']['error'] == 1) {$text = "Votre images est trop volumineuse !";header("Location: ../../index.php?view=admin_sha&sub=categorie&warning=add-categorie&text=$text");}
-        if($_FILES['images_cat']['error'] == 2) {$text = "Votre images est trop volumineuse !";header("Location: ../../index.php?view=admin_sha&sub=categorie&warning=add-categorie&text=$text");}
-        if($_FILES['images_cat']['error'] == 3) {$text = "Téléchargement incomplet !";header("Location: ../../index.php?view=admin_sha&sub=categorie&warning=add-categorie&text=$text");}
-        if($_FILES['images_cat']['error'] == 6) {$text = "Fichier Temporaire Manquant !";header("Location: ../../index.php?view=admin_sha&sub=categorie&warning=add-categorie&text=$text");}
-        if($_FILES['images_cat']['error'] == 7) {$text = "Impossible d'écrire les droits sur le fichier envoyer !";header("Location: ../../index.php?view=admin_sha&sub=categorie&warning=add-categorie&text=$text");}
-        die();
     }
 
     $sql = $DB->execute("INSERT INTO categorie(id, designation_cat, images_cat) VALUES (NULL, :designation_cat, :images_cat)", array(
