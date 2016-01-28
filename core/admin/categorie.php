bb347cfa074ebd2b3694a119aa6e5d7c80f4149f
CRIDIP-SWD <mmockelyn@cridip.com>
2016-01-28 10:10:10 +0100

    GAMESHOP UPDATE

--- core/admin/categorie.php
+++ core/admin/categorie.php
@@ -25,20 +25,28 @@ if(isset($_POST['action']) && $_POST['action'] == 'add-categories')
             }
 
         }
-    }
 
-    $sql = $DB->execute("INSERT INTO categorie(id, designation_cat, images_cat) VALUES (NULL, :designation_cat, :images_cat)", array(
-        "designation_cat"       => $designation_cat,
-        "images_cat"            => $images_cat
-    ));
+        $sql = $DB->execute("INSERT INTO categorie(id, designation_cat, images_cat) VALUES (NULL, :designation_cat, :images_cat)", array(
+            "designation_cat"       => $designation_cat,
+            "images_cat"            => $images_cat
+        ));
 
-    if($sql == 1)
-    {
-        $text = "La catégorie <strong>".$designation_cat."</strong> à bien été créer";
-        header("Location: ../../index.php?view=admin_sha&sub=categories&success=add-categorie&text=$text");
+        if($sql == 1)
+        {
+            $text = "La catégorie <strong>".$designation_cat."</strong> à bien été créer";
+            header("Location: ../../index.php?view=admin_sha&sub=categories&success=add-categorie&text=$text");
+        }else{
+            $text = "Une Erreur à eu lieu lors de la création de la catégorie";
+            header("Location: ../../index.php?view=admin_sha&sub=categories&error=add-categorie&text=$text");
+        }
     }else{
-        $text = "Une Erreur à eu lieu lors de la création de la catégorie";
-        header("Location: ../../index.php?view=admin_sha&sub=categories&error=add-categorie&text=$text");
+        if($_FILES['images_cat']['error'] == 1) {$text = "Votre images est trop volumineuse !";header("Location: ../../index.php?view=admin_sha&sub=categorie&warning=add-categorie&text=$text");}
+        if($_FILES['images_cat']['error'] == 2) {$text = "Votre images est trop volumineuse !";header("Location: ../../index.php?view=admin_sha&sub=categorie&warning=add-categorie&text=$text");}
+        if($_FILES['images_cat']['error'] == 3) {$text = "Téléchargement incomplet !";header("Location: ../../index.php?view=admin_sha&sub=categorie&warning=add-categorie&text=$text");}
+        if($_FILES['images_cat']['error'] == 4) {$text = "Pas de Fichier !";header("Location: ../../index.php?view=admin_sha&sub=categorie&warning=add-categorie&text=$text");}
+        if($_FILES['images_cat']['error'] == 6) {$text = "Fichier Temporaire Manquant !";header("Location: ../../index.php?view=admin_sha&sub=categorie&warning=add-categorie&text=$text");}
+        if($_FILES['images_cat']['error'] == 7) {$text = "Impossible d'écrire les droits sur le fichier envoyer !";header("Location: ../../index.php?view=admin_sha&sub=categorie&warning=add-categorie&text=$text");}
+        die();
     }
 
 
