36f97fc25533f72f5f9f83de1b1dba0893c8019c
CRIDIP-SWD <mmockelyn@cridip.com>
2016-01-29 18:44:55 +0100

    GAMESHOP UPDATE

--- view/admin/index.php
+++ view/admin/index.php
@@ -1123,6 +1123,10 @@
                                             <img class="img-responsive" src="<?= $constante->getUrl(array(), false, true); ?>produit/cards/PS4B.jpg" width="250">
                                         </a>
                                     </div>
+                                    <div class="product_info">
+                                        <div class="product_info_title">BATTLEBORN</div>
+                                        <div class="product_info_categorie">PS4 - <small>Jeux</small></div>
+                                    </div>
                                 </div>
                             </div>
                         </div>
