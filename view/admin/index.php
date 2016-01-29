97eb27737957d5924a13c40d0a8f196fae124ab7
CRIDIP-SWD <mmockelyn@cridip.com>
2016-01-29 18:18:05 +0100

    GAMESHOP UPDATE

--- view/admin/index.php
+++ view/admin/index.php
@@ -1087,6 +1087,178 @@
 <?php endif; ?>
 <?php if(isset($_GET['sub']) && $_GET['sub'] == 'produits'): ?>
     <?php if(!isset($_GET['data'])): ?>
+        <section role="main" class="content-body">
+            <header class="page-header">
+                <h2><i class="fa fa-search"></i> PRODUITS</h2>
 
+                <div class="right-wrapper pull-right">
+                    <ol class="breadcrumbs">
+                        <li>
+                            <a href="index.php?view=admin_sha">
+                                <i class="fa fa-home"></i>
+                            </a>
+                        </li>
+                        <li><span>Produits</span></li>
+                    </ol>
+
+                    <a class="sidebar-right-toggle" data-open="sidebar-right"><i class="fa fa-chevron-left"></i></a>
+                </div>
+            </header>
+
+            <!-- start: page -->
+            <div class="row">
+                <section class="panel panel-primary">
+                    <header class="panel-heading">
+                        <div class="panel-actions">
+                            <a href="index.php?view=admin_sha&sub=add-produit" class="mb-xs mt-xs mr-xs modal-basic panel-action" data-toggle="tooltip" data-original-title="Ajouter un Produit"><i class="fa fa-plus-circle fa-lg"></i></a>
+                        </div>
+                        <h2 class="panel-title">Liste des Catégories</h2>
+                    </header>
+                    <div class="panel-body">
+                        <div class="table-responsive">
+                            <table class="table table-bordered">
+                                <thead>
+                                <tr>
+                                    <th>ID</th>
+                                    <th>Désignation</th>
+                                    <th>Sous catégories</th>
+                                    <th>Action</th>
+                                </tr>
+                                </thead>
+                                <tbody>
+                                <?php
+                                $sql_cat = $DB->query("SELECT * FROM categorie ORDER BY id ASC");
+                                foreach($sql_cat as $cat):
+                                    ?>
+                                    <tr>
+                                        <td><?= $cat->id; ?></td>
+                                        <td><?= html_entity_decode($cat->designation_cat); ?></td>
+                                        <td>
+                                            <table style="width: 100%;">
+                                                <?php
+                                                $sql_sub = $DB->query("SELECT * FROM subcategorie WHERE idcategorie = :idcategorie", array(
+                                                    "idcategorie"   => $cat->id
+                                                ));
+                                                foreach($sql_sub as $sub):
+                                                    ?>
+                                                    <tr>
+                                                        <td><?= html_entity_decode($sub->designation_subcat); ?></td>
+                                                        <td><a href="core/admin/categorie.php?action=supp-subcategorie&idsubcategorie=<?= $sub->id; ?>"><i class="fa fa-trash text-danger"></i></a></td>
+                                                    </tr>
+                                                <?php endforeach; ?>
+                                            </table>
+                                        </td>
+                                        <td>
+                                            <a href="core/admin/categorie.php?action=supp-categorie&idcategorie=<?= $cat->id; ?>" class="btn btn-danger"><i class="fa fa-trash"></i></a>
+                                        </td>
+                                    </tr>
+                                <?php endforeach; ?>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </section>
+            </div>
+            <div id="add-categorie" class="modal-block modal-block-lg modal-header-color modal-block-primary mfp-hide">
+                <section class="panel">
+                    <header class="panel-heading">
+                        <h2 class="panel-title">Nouvelle Catégorie</h2>
+                    </header>
+                    <form id="summary-form" class="form-horizontal" action="core/admin/categorie.php" method="post" enctype="multipart/form-data">
+                        <div class="panel-body">
+                            <div class="modal-wrapper">
+                                <div class="validation-message">
+                                    <ul></ul>
+                                </div>
+
+                                <div class="form-group">
+                                    <label class="col-md-3 control-label" for="cat">Désignation <span class="required">*</span></label>
+                                    <div class="col-md-9">
+                                        <input type="text" id="cat" class="form-control" name="designation_cat" required title="Champs Requis" />
+                                    </div>
+                                </div>
+
+                                <div class="form-group">
+                                    <label class="col-md-3 control-label" for="cat">Images de la catégorie</label>
+                                    <div class="col-md-9">
+                                        <div class="fileupload fileupload-new" data-provides="fileupload">
+                                            <div class="input-append">
+                                                <div class="uneditable-input">
+                                                    <i class="fa fa-file fileupload-exists"></i>
+                                                    <span class="fileupload-preview"></span>
+                                                </div>
+                                                <span class="btn btn-default btn-file">
+                                                    <span class="fileupload-exists">Changer de Fichier</span>
+                                                    <span class="fileupload-new">Sélectionner un fichier</span>
+                                                    <input type="file" name="images_cat"/>
+                                                </span>
+                                                <a href="#" class="btn btn-default fileupload-exists" data-dismiss="fileupload">Supprimer</a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+
+                            </div>
+                        </div>
+                        <footer class="panel-footer">
+                            <div class="row">
+                                <div class="col-md-12 text-right">
+                                    <button class="btn btn-primary" type="submit" name="action" value="add-categories">Valider</button>
+                                    <button class="btn btn-default modal-dismiss">Annuler</button>
+                                </div>
+                            </div>
+                        </footer>
+                    </form>
+                </section>
+            </div>
+            <div id="add-subcategorie" class="modal-block modal-block-lg modal-header-color modal-block-primary mfp-hide">
+                <section class="panel">
+                    <header class="panel-heading">
+                        <h2 class="panel-title">Nouvelle Sous Catégorie</h2>
+                    </header>
+                    <form id="summary-form" class="form-horizontal" action="core/admin/categorie.php" method="post" enctype="multipart/form-data">
+                        <div class="panel-body">
+                            <div class="modal-wrapper">
+                                <div class="validation-message">
+                                    <ul></ul>
+                                </div>
+
+                                <div class="form-group">
+                                    <label class="col-md-3 control-label">Catégorie <span class="required">*</span></label>
+                                    <div class="col-md-9">
+                                        <select data-plugin-selectTwo class="form-control populate" require name="idcategorie">
+                                            <?php
+                                            $sql_cat = $DB->query("SELECT * FROM categorie");
+                                            foreach($sql_cat as $cat):
+                                                ?>
+                                                <option value="<?= $cat->id; ?>"><?= html_entity_decode($cat->designation_cat); ?></option>
+                                            <?php endforeach; ?>
+                                        </select>
+                                    </div>
+                                </div>
+
+                                <div class="form-group">
+                                    <label class="col-md-3 control-label" for="cat">Désignation <span class="required">*</span></label>
+                                    <div class="col-md-9">
+                                        <input type="text" id="cat" class="form-control" name="designation_subcat" required title="Champs Requis" />
+                                    </div>
+                                </div>
+
+
+                            </div>
+                        </div>
+                        <footer class="panel-footer">
+                            <div class="row">
+                                <div class="col-md-12 text-right">
+                                    <button class="btn btn-primary" type="submit" name="action" value="add-subcategorie">Valider</button>
+                                    <button class="btn btn-default modal-dismiss">Annuler</button>
+                                </div>
+                            </div>
+                        </footer>
+                    </form>
+                </section>
+            </div>
+            <!-- end: page -->
+        </section>
     <?php endif; ?>
 <?php endif; ?>
