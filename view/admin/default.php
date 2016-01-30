61d7d3d4c116b465cdd09fc67950511b93b72b29
CRIDIP-SWD <mmockelyn@cridip.com>
2016-01-30 02:59:54 +0100

    GAMESHOP SQUAD

--- view/admin/default.php
+++ view/admin/default.php
@@ -325,7 +325,7 @@ if(!isset($_SESSION['logged']))
 </script>
 <script type="text/javascript">
     function calcul(){
-        var prix_vente = document.getElementById('prix_vente').value();
+        var prix_vente = parseFloat(document.getElementById("Produit").elements['prix_vente'].value);
         var revenue_point = document.getElementById('revenue_point').value();
         var cout_point = document.getElementById('cout_point').value();
 
