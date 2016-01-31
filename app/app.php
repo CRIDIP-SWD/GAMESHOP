981c5a63f82a55ed3bdb6cb5e37e9c2129c19643
CRIDIP-SWD <mmockelyn@cridip.com>
2016-01-31 23:55:33 +0100

    GMS

--- app/app.php
+++ app/app.php
@@ -381,6 +381,19 @@ class ssh2 extends app
             {
                 $text = "Erreur SSH2: Connexion echoué, veuillez vérifier les paramêtre de l'application app/app.php <i>(CLASS)</i>.";
                 header("Location: ../index.php?view=admin_sha&sub=error&text=$text");
+            }else{
+                return $connect;
+            }
+        }else{
+            $connect = ssh2_connect($server, $port);
+            $login = ssh2_auth_password($connect, $user, $pass);
+
+            if(!$login)
+            {
+                $text = "Erreur SSH2: Connexion echoué, veuillez vérifier les paramêtre de l'application app/app.php <i>(CLASS)</i>.";
+                header("Location: ../index.php?view=admin_sha&sub=error&text=$text");
+            }else{
+                return $connect;
             }
         }
     }
