1a5152587cce7e39fddd56448f8c696ac7cfdd28
CRIDIP-SWD <mmockelyn@cridip.com>
2016-01-31 23:11:12 +0100

    GMS

--- core/admin/produit.php
+++ core/admin/produit.php
@@ -486,7 +486,7 @@ if(isset($_POST['action']) && $_POST['action'] == 'add-stock')
 if(isset($_POST['action']) && $_POST['action'] == 'add-images')
 {
     require "../../app/classe.php";
-    var_dump($_POST);
+    var_dump($_FILES);
     die();
     $ref_produit = $_POST['ref_produit'];
     $name_image = $_POST['name_images'];
