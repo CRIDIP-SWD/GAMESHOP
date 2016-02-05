2b343e5ce3dd71d8ff3c259f99d83e89015ad8bf
CRIDIP-SWD <mmockelyn@cridip.com>
2016-02-05 18:51:20 +0100

    gms

--- view/default.php
+++ view/default.php
@@ -142,7 +142,7 @@ ini_set('display_errors', 1);
                 ============================================= -->
                 <div id="logo">
                     <a href="index.php?view=index" class="standard-logo" data-dark-logo="images/logo-dark.png"><img src="assets/images/logo.png" alt="Canvas Logo"></a>
-                    <a href="index.html" class="retina-logo" data-dark-logo="images/logo-dark@2x.png"><img src="images/logo@2x.png" alt="Canvas Logo"></a>
+                    <a href="index.html" class="retina-logo" data-dark-logo="images/logo-dark@2x.png"><img src="assets/images/logo.png" alt="Canvas Logo"></a>
                 </div><!-- #logo end -->
 
                 <!-- Primary Navigation
