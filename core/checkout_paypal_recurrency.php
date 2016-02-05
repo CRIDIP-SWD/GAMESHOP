e05cce8d0e12a101cef2fad4464e5d804a876059
CRIDIP-SWD <mmockelyn@cridip.com>
2016-02-05 02:17:29 +0100

    GAMESHOP

--- core/checkout_paypal_recurrency.php
+++ core/checkout_paypal_recurrency.php
@@ -37,155 +37,19 @@ if($response)
             $paiement = $paypal->request('CreateRecurringPaymentsProfile', array(
                 "TOKEN"                     => $_GET['token'],
                 "PAYERID"                   => $_GET['PayerID'],
-                "PAYMENTACTION"             => 'Sale',
-                "PAYMENTREQUEST_0_CUSTOM"   => $num_commande,
-                "PAYMENTREQUEST_0_AMT"      => $response['AMT'],
-                "PAYMENTREQUEST_0_CURRENCYCODE"  => "EUR",
 
-                "PROFILESTARTDATE"  => $date_format->formatage("Y-m-d"."'T'"."")
+                "PROFILESTARTDATE"  => gmdate("Y-m-dTH:i:sZ", time()+86400),
+                "PROFILEREFERENCE"  => $num_commande,
+                "BILLINGPERIOD"     => "Month",
+                "BILLINGFREQUENCY"  => "3",
+                "AMT"               => $cmd[0]->total_commande + $cmd[0]->prix_envoie,
+
             ));
 
             if($paiement)
             {
-                $transactionID = $paiement['PAYMENTINFO_0_TRANSACTIONID'];
-
-
-                $preco = $DB->count("SELECT COUNT(commande_article.ref_produit) FROM commande_article, produits WHERE commande_article.ref_produit = produits.ref_produit AND produits.statut_global = 2 AND commande_article.num_commande = :num_commande", array(
-                    "num_commande" => $num_commande
-                ));
-
-                $stock = $DB->count("SELECT COUNT(commande_article.ref_produit) FROM commande_article, produits WHERE commande_article.ref_produit = produits.ref_produit AND produits.statut_stock != 2 AND commande_article.num_commande = :num_commande", array(
-                    "num_commande"  => $num_commande
-                ));
-
-                $sql_article = $DB->query("SELECT * FROM commande_article, produits WHERE commande_article.ref_produit = produits.ref_produit AND num_commande = :num_commande", array(
-                    "num_commande"  => $num_commande
-                ));
-
-                if($preco != 0)
-                {
-                    $new_point = $produit_cls->calcRevenuePointClient($num_commande, $cmd[0]->idclient);
-                    $update = $DB->execute("UPDATE commande SET methode_paiement = :methode_paiement, statut = :statut WHERE num_commande = :num_commande", array(
-                        "methode_paiement"      => "PAYPAL EUROPE",
-                        "num_commande"          => $num_commande,
-                        "statut"                => 2
-                    ));
-                    $update = $DB->execute("UPDATE client SET point = :point WHERE idclient = :idclient", array(
-                        "point"     => $new_point,
-                        "idclient"  => $cmd[0]->idclient
-                    ));
-                    $reglement = $DB->execute("INSERT INTO commande_reglement(idreglement, num_commande, mode_reglement, date_reglement, ref_reglement, montant_reglement, etat_reglement)
-                                              VALUES (NULL, :num_commande, :mode_reglement, :date_reglement, :ref_reglement, :montant_reglement, :etat_reglement)", array(
-                        "num_commande"      => $num_commande,
-                        "mode_reglement"    => "PAYPAL EUROPE",
-                        "date_reglement"    => $date_format->format_strt(date("d-m-Y H:i:s")),
-                        "ref_reglement"     => $transactionID,
-                        "montant_reglement" => $paiement['PAYMENTINFO_0_AMT'],
-                        "etat_reglement"    => 1
-                    ));
-                    $error = "Impossible de Définir le réglement en base de donnée.<br>Veuillez contactez l'administrateur système.";
-                    if($update >= 1 AND $reglement == 1)
-                    {
-                        $update = $DB->execute("UPDATE commande SET statut = :statut WHERE num_commande = :num_commande", array(
-                            "statut" => 3,
-                            "num_commande" => $num_commande
-                        ));
-                        header("Location: ../index.php?view=checkout&sub=recap&num_commande=$num_commande");
-                    }else{
-                        $update = $DB->execute("UPDATE commande SET statut = :statut WHERE num_commande = :num_commande", array(
-                            "statut" => 6,
-                            "num_commande" => $num_commande
-                        ));
-                        header("Location: ../index.php?view=checkout&sub=paiement&num_commande=$num_commande&error=critical&data=$error");
-                    }
-                }elseif($stock != 0)
-                {
-                    $new_point = $produit_cls->calcRevenuePointClient($num_commande, $cmd[0]->idclient);
-                    $update = $DB->execute("UPDATE commande SET methode_paiement = :methode_paiement, statut = :statut WHERE num_commande = :num_commande", array(
-                        "methode_paiement"      => "PAYPAL EUROPE",
-                        "num_commande"          => $num_commande,
-                        "statut"                => 2
-                    ));
-                    $update = $DB->execute("UPDATE client SET point = :point WHERE idclient = :idclient", array(
-                        "point"     => $new_point,
-                        "idclient"  => $cmd[0]->idclient
-                    ));
-                    $reglement = $DB->execute("INSERT INTO commande_reglement(idreglement, num_commande, mode_reglement, date_reglement, ref_reglement, montant_reglement, etat_reglement)
-                                              VALUES (NULL, :num_commande, :mode_reglement, :date_reglement, :ref_reglement, :montant_reglement, :etat_reglement)", array(
-                        "num_commande"      => $num_commande,
-                        "mode_reglement"    => "PAYPAL EUROPE",
-                        "date_reglement"    => $date_format->format_strt(date("d-m-Y H:i:s")),
-                        "ref_reglement"     => $transactionID,
-                        "montant_reglement" => $paiement['PAYMENTINFO_0_AMT'],
-                        "etat_reglement"    => 1
-                    ));
-                    $error = "Impossible de Définir le réglement en base de donnée.<br>Veuillez contactez l'administrateur système.";
-                    if($update >= 1 AND $reglement == 1)
-                    {
-                        $update = $DB->execute("UPDATE commande SET statut = :statut WHERE num_commande = :num_commande", array(
-                            "statut" => 3,
-                            "num_commande" => $num_commande
-                        ));
-                        header("Location: ../index.php?view=checkout&sub=recap&num_commande=$num_commande");
-                    }else{
-                        $update = $DB->execute("UPDATE commande SET statut = :statut WHERE num_commande = :num_commande", array(
-                            "statut" => 6,
-                            "num_commande" => $num_commande
-                        ));
-                        header("Location: ../index.php?view=checkout&sub=paiement&num_commande=$num_commande&error=critical&data=$error");
-                    }
-                }else{
-                    $new_point = $produit_cls->calcRevenuePointClient($num_commande, $cmd[0]->idclient);
-                    $update = $DB->execute("UPDATE commande SET methode_paiement = :methode_paiement, statut = :statut WHERE num_commande = :num_commande", array(
-                        "methode_paiement"      => "PAYPAL EUROPE",
-                        "num_commande"          => $num_commande,
-                        "statut"                => 2
-                    ));
-                    $update = $DB->execute("UPDATE client SET point = :point WHERE idclient = :idclient", array(
-                        "point"     => $new_point,
-                        "idclient"  => $idclient
-                    ));
-                    foreach($sql_article as $article){
-                        $ref_produit = $article->ref_produit;
-                        $new_stock = $article->stock - $article->qte;
-                        $update = $DB->execute("UPDATE produits SET stock = :stock WHERE ref_produit = :ref_produit", array(
-                            "stock"     => $new_stock,
-                            "ref_produit"   => $ref_produit
-                        ));
-                        if($article->stock <= 0)
-                        {
-                            $update = $DB->execute("UPDATE produits SET statut_stock = :statut WHERE ref_produit = :ref_produit", array(
-                                "statut"        => 0,
-                                "ref_produit"   => $ref_produit
-                            ));
-                        }
-                    }
-                    $reglement = $DB->execute("INSERT INTO commande_reglement(idreglement, num_commande, mode_reglement, date_reglement, ref_reglement, montant_reglement, etat_reglement)
-                                              VALUES (NULL, :num_commande, :mode_reglement, :date_reglement, :ref_reglement, :montant_reglement, :etat_reglement)", array(
-                        "num_commande"      => $num_commande,
-                        "mode_reglement"    => "PAYPAL EUROPE",
-                        "date_reglement"    => $date_format->format_strt(date("d-m-Y H:i:s")),
-                        "ref_reglement"     => $transactionID,
-                        "montant_reglement" => $paiement['PAYMENTINFO_0_AMT'],
-                        "etat_reglement"    => 1
-                    ));
-                    $error = "Impossible de Définir le réglement en base de donnée.<br>Veuillez contactez l'administrateur système.";
-                    if($update >= 1 AND $reglement == 1)
-                    {
-                        $update = $DB->execute("UPDATE commande SET statut = :statut WHERE num_commande = :num_commande", array(
-                            "statut" => 3,
-                            "num_commande" => $num_commande
-                        ));
-                        header("Location: ../index.php?view=checkout&sub=recap&num_commande=$num_commande");
-                    }else{
-                        $update = $DB->execute("UPDATE commande SET statut = :statut WHERE num_commande = :num_commande", array(
-                            "statut" => 6,
-                            "num_commande" => $num_commande
-                        ));
-                        header("Location: ../index.php?view=checkout&sub=paiement&num_commande=$num_commande&error=critical&data=$error");
-                    }
-
-                }
+                var_dump($paiement);
+                die();
             }else{
                 var_dump($paypal->errors);
                 die();
