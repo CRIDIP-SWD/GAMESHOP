e92887af5e410c6cc303aa950ad9c19c5d86bc68
CRIDIP-SWD <mmockelyn@cridip.com>
2016-02-05 02:09:06 +0100

    GAMESHOP

--- view/default.php
+++ view/default.php
@@ -1,6 +1,6 @@
 <?php
 ini_set('display_errors', 1);
-echo $date_format->formatage("Y-m-d"."'T'"."H:i:s"."'Z'", 1454716392);
+echo gmdate("M d Y H:i:s", mktime(0, 0, 0, 1, 1, 1998));
 die();
 ?>
 <!DOCTYPE html>
